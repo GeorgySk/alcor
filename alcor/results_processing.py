6003db5a32f74066eec81f19e7cb95d80b00473f
LostFan123 <skorobogatov@phystech.edu>
2017-05-15 09:55:39 +0200

    Added loop over stars

--- alcor/results_processing.py
+++ alcor/results_processing.py
@@ -4,7 +4,7 @@ from decimal import Decimal
 
 
 def main() -> None:
-    list_of_stars = []
+    stars = []
     Star = namedtuple('Star', 'luminosity '
                               'proper_motion '
                               'declination ' 
@@ -16,14 +16,13 @@ def main() -> None:
                               'velocity_v '
                               'velocity_w ')
 
-    pi = 4.0 * atan(1.0)
-
     # Binning parameters of Luminosity Function
     min_bolometric_magnitude = 6.0
     max_bolometric_magnitude = 21.0
     bin_size = 0.5
-    number_of_bins = (max_bolometric_magnitude - min_bolometric_magnitude)\
-                     /bin_size
+    bolometric_magnitude_amplitude = (max_bolometric_magnitude
+                                      - min_bolometric_magnitude)
+    bins_count = bolometric_magnitude_amplitude / bin_size
 
     with open('output.dat', 'r') as f:
         line_counter = 0
@@ -32,7 +31,7 @@ def main() -> None:
             parts = line.split()
             params = map(Decimal, parts)
             star = Star(*params)
-            list_of_stars.append(star)
+            stars.append(star)
 
     elemination_counters = {'eliminated_by_parallax': 0,
                             'eliminated_by_declination': 0,
@@ -40,6 +39,17 @@ def main() -> None:
                             'eliminated_by_proper_motion': 0,
                             'eliminated_by_reduced_proper_motion': 0,
                             'eliminated_by_apparent_magnitude': 0}
+
+    for i in range(0, size(stars)):
+        star_is_eleminated = False
+        apply_elemination_criteria(stars(i), star_is_eleminated, elemination_counters)
+        if not star_is_eleminated:
+            # Save stars after applying elemination criteria
+            set_radial_velocity_to_zero(stars(i))
+            distribute_into_bins(star, bins)
+
+            
+
 
 
 if __name__ == '__main__':
