da2a7835da902889d1e3dc2ac53a66358e0136b8
LostFan123 <skorobogatov@phystech.edu>
2017-05-22 17:06:22 +0200

    Deleted process_settings dict

--- alcor/services/results_processing/sampling.py
+++ alcor/services/results_processing/sampling.py
@@ -4,6 +4,8 @@ from fractions import Fraction
 
 from math import log10
 
+from cassandra.deserializers import Decimal
+
 from alcor.models.star import Star
 
 MIN_PARALLAX = 0.025
@@ -13,64 +15,64 @@ MIN_PROPER_MOTION = 0.04
 
 
 def check_elimination(star: Star,
-                      elimination_counters: Counter,
+                      eliminations_counter: Counter,
                       method: str) -> bool:
     # TODO: implement pc/kpc units
-    galactocentric_distance = star.galactocentric_distance * 10e3
+    galactocentric_distance = star.galactocentric_distance * Decimal(10e3)
     parallax = Fraction(1.0, Fraction(galactocentric_distance))
     # TODO: find out the meaning of the following constants
     hrm = star.go_photometry + 5.0 * log10(star.proper_motion) + 5.0
     gz = star.gr_photometry + star.rz_photometry
 
     if parallax < MIN_PARALLAX:
-        elimination_counters['parallax'] += 1
+        eliminations_counter['parallax'] += 1
         return True
     elif star.declination < MIN_DECLINATION:
-        elimination_counters['declination'] += 1
+        eliminations_counter['declination'] += 1
         return True
     elif (star.velocity_u ** 2 + star.velocity_v ** 2 + star.velocity_z ** 2
           > MAX_VELOCITY ** 2):
-        elimination_counters['velocity'] += 1
+        eliminations_counter['velocity'] += 1
         return True
     elif method == 'restricted':
         if star.proper_motion < MIN_PROPER_MOTION:
-            elimination_counters['proper_motion'] += 1
+            eliminations_counter['proper_motion'] += 1
             return True
         # TODO: find out the meaning of the following constants
         elif gz < -0.33 and hrm < 14.0:
-            elimination_counters['reduced_proper_motion'] += 1
+            eliminations_counter['reduced_proper_motion'] += 1
             return True
         # TODO: find out the meaning of the following constants
         elif hrm < 3.559 * gz + 15.17:
-            elimination_counters['reduced_proper_motion'] += 1
+            eliminations_counter['reduced_proper_motion'] += 1
             return True
         # TODO: find out the meaning of the following constant
         elif star.v_photometry >= 19.0:
-            elimination_counters['apparent_magnitude'] += 1
+            eliminations_counter['apparent_magnitude'] += 1
             return True
     return False
 
 
-def write_elimination_stats(full_sample_stars_count: int,
-                            restricted_sample_stars_count: int,
-                            elimination_counters: Counter) -> None:
+def write_elimination_stats(raw_sample_stars_count: int,
+                            filtered_stars_count: int,
+                            eliminations_counter: Counter) -> None:
     with open('elimination_stats.csv', 'w') as csv_file:
         file_writer = csv.writer(csv_file, delimiter='  ')
-        file_writer.writerow('Initial number of WDs:',
-                             'Eliminated by parallax:',
-                             'Eliminated by declination:',
-                             'Initial number of stars in northern hemisphere:',
-                             'Eliminated by proper motion:',
-                             'Eliminated by reduced proper motion:',
-                             'Eliminated by apparent magnitude:',
-                             'Number of stars in restricted sample:')
-        file_writer.writerow(full_sample_stars_count,
-                             elimination_counters['parallax'],
-                             elimination_counters['declination'],
-                             full_sample_stars_count
-                             - elimination_counters['parallax']
-                             - elimination_counters['declination'],
-                             elimination_counters['proper_motion'],
-                             elimination_counters['reduced_proper_motion'],
-                             elimination_counters['apparent_magnitude'],
-                             restricted_sample_stars_count)
+        file_writer.writerow('RawNº',
+                             'Par-x',
+                             'Dec',
+                             'NH Nº',
+                             'PropM',
+                             'RedPrM',
+                             'AppMag',
+                             'RestrNº')
+        file_writer.writerow(raw_sample_stars_count,
+                             eliminations_counter['parallax'],
+                             eliminations_counter['declination'],
+                             raw_sample_stars_count
+                             - eliminations_counter['parallax']
+                             - eliminations_counter['declination'],
+                             eliminations_counter['proper_motion'],
+                             eliminations_counter['reduced_proper_motion'],
+                             eliminations_counter['apparent_magnitude'],
+                             filtered_stars_count)
--- alcor/services/results_processing/service.py
+++ alcor/services/results_processing/service.py
@@ -1,7 +1,8 @@
+import uuid
+
 from collections import Counter
 from functools import partial
 from itertools import filterfalse
-from typing import Dict
 
 from .kinematics import (write_velocity_clouds_data,
                          write_velocities_vs_magnitude_data)
@@ -11,41 +12,41 @@ from .sampling import (write_elimination_stats,
 from alcor.utils import parse_stars
 
 
-def run_processing(*, settings: Dict[str, str]) -> None:
-    input_path = settings['input_path']
-    with open(input_path, 'r') as input_file:
-        raw_stars_sample = list(parse_stars(input_file, ''))
+def run_processing(data_path,
+                   luminosity_function,
+                   velocity_clouds,
+                   velocities_vs_magnitude,
+                   sample,
+                   nullify_radial_velocity,
+                   lepine_criterion) -> None:
+    group_id = uuid.uuid4()
+    with open(data_path, 'r') as input_file:
+        stars = list(parse_stars(input_file, group_id))
 
-    methods = settings['methods']
-    plots = settings['plots']
-
-    sampling_method = methods['sampling_method']
-    eliminations_counter = Counter(int)
+    eliminations_counter = Counter()
     apply_elimination_criteria = partial(
         check_elimination,
         eliminations_counter=eliminations_counter,
-        method=sampling_method)
-    if sampling_method == 'full':
-        stars = filterfalse(apply_elimination_criteria,
-                            raw_stars_sample)
-    elif sampling_method == 'restricted':
-        stars = filterfalse(apply_elimination_criteria,
-                            raw_stars_sample)
-    write_elimination_stats(len(raw_stars_sample),
-                            len(stars),
-                            eliminations_counter)
+        method=sample)
+    raw_sample_stars_count = len(stars)
+    if sample == 'full' or sample == 'restricted':
+        filtered_stars = filterfalse(apply_elimination_criteria,
+                                     stars)
+    write_elimination_stats(raw_sample_stars_count=raw_sample_stars_count,
+                            filtered_stars_count=len(list(filtered_stars)),
+                            eliminations_counter=eliminations_counter)
 
-    if methods['nullify_radial_velocity']:
-        for star in stars:
-            star.set_radial_velocity_to_zero()
+    if nullify_radial_velocity:
+        for filtered_stars in stars:
+            filtered_stars.set_radial_velocity_to_zero()
 
-    lepine_criterion_applied = methods['lepine_criterion_applied']
+    lepine_criterion_applied = lepine_criterion
 
-    if plots['luminosity_function']:
-        write_luminosity_function_data(stars)
-    if plots['velocity_clouds']:
-        write_velocity_clouds_data(stars,
+    if luminosity_function:
+        write_luminosity_function_data(filtered_stars)
+    if velocity_clouds:
+        write_velocity_clouds_data(filtered_stars,
                                    lepine_criterion_applied)
-    if plots['velocities_vs_magnitude']:
-        write_velocities_vs_magnitude_data(stars,
+    if velocities_vs_magnitude:
+        write_velocities_vs_magnitude_data(filtered_stars,
                                            lepine_criterion_applied)
