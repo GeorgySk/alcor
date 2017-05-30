ae069f1f8b862945b619a26b9c9800adc4f7cb90
LostFan123 <skorobogatov@phystech.edu>
2017-05-30 13:10:49 +0200

    Added Lepine's case for velocities vs magnitude. Inconsistent results

--- alcor/services/plotting/velocities_vs_magnitude.py
+++ alcor/services/plotting/velocities_vs_magnitude.py
@@ -9,8 +9,8 @@ from bokeh.plotting import (figure,
 def plot() -> None:
     output_file("velocities_vs_magnitude.html")
 
-    with open('uvw_vs_mag_bins.csv', 'r') as data_file:
-        reader = csv.reader(data_file,
+    with open('uvw_vs_mag_bins.csv', 'r') as file:
+        reader = csv.reader(file,
                             delimiter=' ')
 
         header_row = next(reader)
@@ -80,8 +80,8 @@ def plot() -> None:
     bottom_plot.multi_line(multiline_x,
                            multiline_y_w)
 
-    with open('uvw_vs_mag_cloud.csv', 'r') as data_file:
-        reader = csv.reader(data_file,
+    with open('uvw_vs_mag_cloud.csv', 'r') as file:
+        reader = csv.reader(file,
                             delimiter=' ')
 
         header_row = next(reader)
@@ -113,4 +113,157 @@ def plot() -> None:
 
 
 def plot_lepine_case():
-    pass
+    output_file("velocities_vs_magnitude.html")
+    with open('u_vs_mag_bins.csv', 'r') as file:
+        reader = csv.reader(file,
+                            delimiter=' ')
+
+        header_row = next(reader)
+
+        average_bin_magnitude_u = []
+        average_velocity_u = []
+        velocity_u_std = []
+
+        for row in reader:
+            average_bin_magnitude_u.append(float(row[0]))
+            average_velocity_u.append(float(row[1]))
+            velocity_u_std.append(float(row[2]))
+
+    with open('v_vs_mag_bins.csv', 'r') as file:
+        reader = csv.reader(file,
+                            delimiter=' ')
+
+        header_row = next(reader)
+
+        average_bin_magnitude_v = []
+        average_velocity_v = []
+        velocity_v_std = []
+
+        for row in reader:
+            average_bin_magnitude_v.append(float(row[0]))
+            average_velocity_v.append(float(row[1]))
+            velocity_v_std.append(float(row[2]))
+
+    with open('w_vs_mag_bins.csv', 'r') as file:
+        reader = csv.reader(file,
+                            delimiter=' ')
+
+        header_row = next(reader)
+
+        average_bin_magnitude_w = []
+        average_velocity_w = []
+        velocity_w_std = []
+
+        for row in reader:
+            average_bin_magnitude_w.append(float(row[0]))
+            average_velocity_w.append(float(row[1]))
+            velocity_w_std.append(float(row[2]))
+
+    top_plot = figure(width=500, height=250)
+    top_plot.line(average_bin_magnitude_u,
+                  average_velocity_u)
+    top_plot.square(average_bin_magnitude_u,
+                    average_velocity_u)
+
+    middle_plot = figure(width=500, height=250)
+    middle_plot.line(average_bin_magnitude_v,
+                     average_velocity_v)
+    middle_plot.square(average_bin_magnitude_v,
+                       average_velocity_v)
+
+    bottom_plot = figure(width=500, height=250)
+    bottom_plot.line(average_bin_magnitude_w,
+                     average_velocity_w)
+    bottom_plot.square(average_bin_magnitude_w,
+                       average_velocity_w)
+
+    multiline_x_u = []
+    multiline_x_v = []
+    multiline_x_w = []
+    multiline_y_u = []
+    multiline_y_v = []
+    multiline_y_w = []
+
+    for (magnitude_u, magnitude_v, magnitude_w,
+         velocity_u, velocity_v, velocity_w,
+         std_u, std_v, std_w) in zip(average_bin_magnitude_u,
+                                     average_bin_magnitude_v,
+                                     average_bin_magnitude_w,
+                                     average_velocity_u,
+                                     average_velocity_v,
+                                     average_velocity_w,
+                                     velocity_u_std,
+                                     velocity_v_std,
+                                     velocity_w_std):
+        multiline_x_u.append((magnitude_u,
+                              magnitude_u))
+        multiline_x_v.append((magnitude_v,
+                              magnitude_v))
+        multiline_x_w.append((magnitude_w,
+                              magnitude_w))
+        multiline_y_u.append((velocity_u - std_u,
+                              velocity_u + std_u))
+        multiline_y_v.append((velocity_v - std_v,
+                              velocity_v + std_v))
+        multiline_y_w.append((velocity_w - std_w,
+                              velocity_w + std_w))
+
+    top_plot.multi_line(multiline_x_u,
+                        multiline_y_u)
+    middle_plot.multi_line(multiline_x_v,
+                           multiline_y_v)
+    bottom_plot.multi_line(multiline_x_w,
+                           multiline_y_w)
+
+    with open('u_vs_mag_cloud.csv', 'r') as file:
+        reader = csv.reader(file,
+                            delimiter=' ')
+
+        header_row = next(reader)
+
+        bolometric_magnitude = []
+        velocity_u = []
+
+        for row in reader:
+            bolometric_magnitude.append(float(row[0]))
+            velocity_u.append(float(row[1]))
+
+    with open('v_vs_mag_cloud.csv', 'r') as file:
+        reader = csv.reader(file,
+                            delimiter=' ')
+
+        header_row = next(reader)
+
+        bolometric_magnitude = []
+        velocity_v = []
+
+        for row in reader:
+            bolometric_magnitude.append(float(row[0]))
+            velocity_v.append(float(row[1]))
+
+    with open('w_vs_mag_cloud.csv', 'r') as file:
+        reader = csv.reader(file,
+                            delimiter=' ')
+
+        header_row = next(reader)
+
+        bolometric_magnitude = []
+        velocity_w = []
+
+        for row in reader:
+            bolometric_magnitude.append(float(row[0]))
+            velocity_w.append(float(row[1]))
+
+    top_plot.circle(bolometric_magnitude,
+                    velocity_u,
+                    size=1)
+    middle_plot.circle(bolometric_magnitude,
+                       velocity_v,
+                       size=1)
+    bottom_plot.circle(bolometric_magnitude,
+                       velocity_w,
+                       size=1)
+
+    show(column(top_plot,
+                middle_plot,
+                bottom_plot))
