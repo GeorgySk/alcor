37afe76f0e044b117580932d10055e93f755074d
ace <azatibrakov@gmail.com>
2017-06-05 04:22:55 +0700

    Added fetching by 'AbstractQuerySet' object & 'SelectStatement'

--- alcor/services/data_access/__init__.py
+++ alcor/services/data_access/__init__.py
@@ -1,4 +1,7 @@
 from .creating import insert
 from .statements import (select_statement,
-                         insert_statement)
-from .reading import fetch
+                         insert_statement,
+                         model_insert_statement)
+from .reading import (fetch_query,
+                      fetch_select_statement,
+                      fetch_statement)
--- alcor/services/data_access/reading.py
+++ alcor/services/data_access/reading.py
@@ -1,19 +1,58 @@
 from typing import (Optional,
+                    Dict,
                     List)
 
 from cassandra.cluster import Session
+from cassandra.cqlengine.query import AbstractQuerySet
+from cassandra.cqlengine.statements import SelectStatement
+from cassandra.query import SimpleStatement
 
 from alcor.types import (StatementType,
                          CallbackType,
-                         RecordType)
+                         RecordType,
+                         ColumnValueType)
 from .callbacks import add_callback
+from .statements import query_to_select_statement
 
 
-def fetch(*,
-          statement: StatementType,
-          session: Session,
-          callback: CallbackType = None
-          ) -> Optional[List[RecordType]]:
-    future = session.execute_async(statement)
+def fetch_query(*,
+                query: AbstractQuerySet,
+                session: Session,
+                callback: CallbackType = None,
+                statement_cls=SimpleStatement,
+                **kwargs
+                ) -> Optional[List[RecordType]]:
+    select_statement = query_to_select_statement(query)
+    return fetch_select_statement(select_statement=select_statement,
+                                  session=session,
+                                  callback=callback,
+                                  statement_cls=statement_cls,
+                                  **kwargs)
+
+
+def fetch_select_statement(*,
+                           select_statement: SelectStatement,
+                           session: Session,
+                           callback: CallbackType = None,
+                           statement_cls=SimpleStatement,
+                           **kwargs
+                           ) -> Optional[List[RecordType]]:
+    parameters = select_statement.get_context()
+    statement = statement_cls(str(select_statement),
+                              **kwargs)
+    return fetch_statement(statement=statement,
+                           parameters=parameters,
+                           session=session,
+                           callback=callback)
+
+
+def fetch_statement(*,
+                    statement: StatementType,
+                    parameters: Dict[str, ColumnValueType] = None,
+                    session: Session,
+                    callback: CallbackType = None
+                    ) -> Optional[List[RecordType]]:
+    future = session.execute_async(statement,
+                                   parameters=parameters)
     return add_callback(future=future,
                         callback=callback)
