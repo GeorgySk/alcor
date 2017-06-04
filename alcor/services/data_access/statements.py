a3a63d1a631d6acf07b94f98f8935199c464a57d
ace <azatibrakov@gmail.com>
2017-06-05 04:22:06 +0700

    Added converter from 'AbstractQuerySet' objects to 'SelectStatement's, insert raw query generator based on Cassandra ORM model

--- alcor/services/data_access/statements.py
+++ alcor/services/data_access/statements.py
@@ -1,5 +1,9 @@
 from typing import List
 
+from cassandra.cqlengine.models import Model
+from cassandra.cqlengine.query import AbstractQuerySet
+from cassandra.cqlengine.statements import SelectStatement
+
 from alcor.utils import join_str
 
 ALL_COLUMNS_ALIAS = '*'
@@ -9,7 +13,7 @@ BIND_ALIAS = '?'
 
 def select_statement(*,
                      table_name: str,
-                     columns_names: str = ALL_COLUMNS_ALIAS,
+                     columns_names: List[str] = ALL_COLUMNS_ALIAS,
                      limit: int = None):
     columns_names_str = join_str(columns_names)
     statement = (f'SELECT {columns_names_str} '
@@ -27,3 +31,18 @@ def insert_statement(*,
     binds_aliases_str = join_str(binds_aliases)
     return (f'INSERT INTO {table_name} ({columns_names_str}) '
             f'VALUES ({binds_aliases_str})')
+
+
+def query_to_select_statement(query: AbstractQuerySet,
+                              *,
+                              count: bool = False) -> SelectStatement:
+    select_statement = query._select_query()
+    select_statement.count = count
+    return select_statement
+
+
+def model_insert_statement(model: Model) -> str:
+    table_name = model.column_family_name()
+    columns_names = model().keys()
+    return insert_statement(table_name=table_name,
+                            columns_names=columns_names)
