11ad67320604c1447aa268c283b4931c56be4109
LostFan123 <skorobogatov@phystech.edu>
2017-06-14 17:46:09 +0200

    Added labels for velocity clouds plots

--- alcor/services/plotting/velocity_clouds.py
+++ alcor/services/plotting/velocity_clouds.py
@@ -36,6 +36,15 @@ def plot() -> None:
     middle_plot = figure()
     bottom_plot = figure()
 
+    top_plot.xaxis.axis_label = 'U'
+    top_plot.yaxis.axis_label = 'W'
+
+    middle_plot.xaxis.axis_label = 'U'
+    middle_plot.yaxis.axis_label = 'V'
+
+    bottom_plot.xaxis.axis_label = 'W'
+    bottom_plot.yaxis.axis_label = 'V'
+
     with open('uvw_cloud.csv', 'r') as file:
         reader = csv.reader(file,
                             delimiter=CSV_DELIMITER)
@@ -51,53 +60,53 @@ def plot() -> None:
                        velocity_v,
                        size=1)
 
-    top_ellipse = Ellipse(x=AVERAGE_POPULATION_VELOCITY_U,
-                          y=AVERAGE_POPULATION_VELOCITY_W,
-                          width=STD_POPULATION_U * 2,
-                          height=STD_POPULATION_W * 2,
-                          fill_color='white',
-                          fill_alpha=0.,
-                          line_dash='dashed')
-    top_double_ellipse = Ellipse(x=AVERAGE_POPULATION_VELOCITY_U,
-                                 y=AVERAGE_POPULATION_VELOCITY_W,
-                                 width=STD_POPULATION_U * 4,
-                                 height=STD_POPULATION_W * 4,
-                                 fill_color='white',
-                                 fill_alpha=0.)
-    top_plot.add_glyph(top_ellipse)
-    top_plot.add_glyph(top_double_ellipse)
-
-    middle_ellipse = Ellipse(x=AVERAGE_POPULATION_VELOCITY_U,
-                             y=AVERAGE_POPULATION_VELOCITY_V,
-                             width=STD_POPULATION_U * 2,
-                             height=STD_POPULATION_V * 2,
-                             fill_color='white',
-                             fill_alpha=0.,
-                             line_dash='dashed')
-    middle_double_ellipse = Ellipse(x=AVERAGE_POPULATION_VELOCITY_U,
-                                    y=AVERAGE_POPULATION_VELOCITY_V,
-                                    width=STD_POPULATION_U * 4,
-                                    height=STD_POPULATION_V * 4,
-                                    fill_color='white',
-                                    fill_alpha=0.)
-    middle_plot.add_glyph(middle_ellipse)
-    middle_plot.add_glyph(middle_double_ellipse)
-
-    bottom_ellipse = Ellipse(x=AVERAGE_POPULATION_VELOCITY_V,
-                             y=AVERAGE_POPULATION_VELOCITY_W,
-                             width=STD_POPULATION_U * 2,
-                             height=STD_POPULATION_V * 2,
-                             fill_color='white',
-                             fill_alpha=0.,
-                             line_dash='dashed')
-    bottom_double_ellipse = Ellipse(x=AVERAGE_POPULATION_VELOCITY_V,
-                                    y=AVERAGE_POPULATION_VELOCITY_W,
-                                    width=STD_POPULATION_U * 4,
-                                    height=STD_POPULATION_V * 4,
-                                    fill_color='white',
-                                    fill_alpha=0.)
-    bottom_plot.add_glyph(bottom_ellipse)
-    bottom_plot.add_glyph(bottom_double_ellipse)
+    # top_ellipse = Ellipse(x=AVERAGE_POPULATION_VELOCITY_U,
+    #                       y=AVERAGE_POPULATION_VELOCITY_W,
+    #                       width=STD_POPULATION_U * 2,
+    #                       height=STD_POPULATION_W * 2,
+    #                       fill_color='white',
+    #                       fill_alpha=0.,
+    #                       line_dash='dashed')
+    # top_double_ellipse = Ellipse(x=AVERAGE_POPULATION_VELOCITY_U,
+    #                              y=AVERAGE_POPULATION_VELOCITY_W,
+    #                              width=STD_POPULATION_U * 4,
+    #                              height=STD_POPULATION_W * 4,
+    #                              fill_color='white',
+    #                              fill_alpha=0.)
+    # top_plot.add_glyph(top_ellipse)
+    # top_plot.add_glyph(top_double_ellipse)
+    #
+    # middle_ellipse = Ellipse(x=AVERAGE_POPULATION_VELOCITY_U,
+    #                          y=AVERAGE_POPULATION_VELOCITY_V,
+    #                          width=STD_POPULATION_U * 2,
+    #                          height=STD_POPULATION_V * 2,
+    #                          fill_color='white',
+    #                          fill_alpha=0.,
+    #                          line_dash='dashed')
+    # middle_double_ellipse = Ellipse(x=AVERAGE_POPULATION_VELOCITY_U,
+    #                                 y=AVERAGE_POPULATION_VELOCITY_V,
+    #                                 width=STD_POPULATION_U * 4,
+    #                                 height=STD_POPULATION_V * 4,
+    #                                 fill_color='white',
+    #                                 fill_alpha=0.)
+    # middle_plot.add_glyph(middle_ellipse)
+    # middle_plot.add_glyph(middle_double_ellipse)
+    #
+    # bottom_ellipse = Ellipse(x=AVERAGE_POPULATION_VELOCITY_V,
+    #                          y=AVERAGE_POPULATION_VELOCITY_W,
+    #                          width=STD_POPULATION_U * 2,
+    #                          height=STD_POPULATION_V * 2,
+    #                          fill_color='white',
+    #                          fill_alpha=0.,
+    #                          line_dash='dashed')
+    # bottom_double_ellipse = Ellipse(x=AVERAGE_POPULATION_VELOCITY_V,
+    #                                 y=AVERAGE_POPULATION_VELOCITY_W,
+    #                                 width=STD_POPULATION_U * 4,
+    #                                 height=STD_POPULATION_V * 4,
+    #                                 fill_color='white',
+    #                                 fill_alpha=0.)
+    # bottom_plot.add_glyph(bottom_ellipse)
+    # bottom_plot.add_glyph(bottom_double_ellipse)
 
     main_plot = gridplot(children=[top_plot,
                                    middle_plot,
