355774d506fd1c73d4cebb23d69f491be3a15180
Georgy Skorobogatov <skorobogatov@phystech.edu>
2017-08-21 21:25:00 +0200

    Finished DA_cooling module, checking needed

--- alcor/types.py
+++ alcor/types.py
@@ -5,3 +5,6 @@ NumericType = Union[int, float]
 GridParametersInfoType = Dict[str, Union[NumericType,
                                          Dict[str, NumericType]]]
 CSVParametersInfoType = Dict[str, Dict[str, Union[str, int]]]
+ParametersValuesType = Dict[str, Union[NumericType,
+                                       Dict[str, Union[str, NumericType]]]]
+CoolingSequenceType = Dict[str, Dict[str, np.ndarray]]
