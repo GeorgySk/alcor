8c5ce6132edc80d71209bd703c3c307694e3bcd9
Georgy Skorobogatov <skorobogatov@phystech.edu>
2017-08-21 22:09:59 +0200

    Returned previous version of clouds generating function

--- alcor/services/velocities/utils.py
+++ alcor/services/velocities/utils.py
@@ -1,4 +1,4 @@
-from typing import Union, Tuple, List
+from typing import Union
 
 from alcor.models import Star
 from alcor.models.velocities import (LepineCaseUVCloud,
@@ -6,26 +6,21 @@ from alcor.models.velocities import (LepineCaseUVCloud,
                                      LepineCaseVWCloud)
 
 
-def star_cloud(stars: List[Star]) -> Tuple[List[LepineCaseUVCloud],
-                                           List[LepineCaseUWCloud],
-                                           List[LepineCaseVWCloud]]:
-    uv_cloud = []
-    uw_cloud = []
-    vw_cloud = []
-    for star in stars:
-        highest_coordinate = max(abs(star.coordinate_x),
-                                 abs(star.coordinate_y),
-                                 abs(star.coordinate_z))
-        if abs(star.coordinate_x) == highest_coordinate:
-            vw_cloud.append(LepineCaseVWCloud(group_id=star.group_id,
-                                              velocity_v=star.velocity_v,
-                                              velocity_w=star.velocity_w))
-        elif abs(star.coordinate_y) == highest_coordinate:
-            uw_cloud.append(LepineCaseUWCloud(group_id=star.group_id,
-                                              velocity_u=star.velocity_u,
-                                              velocity_w=star.velocity_w))
-        else:
-            uv_cloud.append(LepineCaseUVCloud(group_id=star.group_id,
-                                              velocity_u=star.velocity_u,
-                                              velocity_v=star.velocity_v))
-    return uv_cloud, uw_cloud, vw_cloud
+def star_cloud(star: Star) -> Union[LepineCaseUVCloud,
+                                    LepineCaseUWCloud,
+                                    LepineCaseVWCloud]:
+    highest_coordinate = max(abs(star.coordinate_x),
+                             abs(star.coordinate_y),
+                             abs(star.coordinate_z))
+    if abs(star.coordinate_x) == highest_coordinate:
+        yield LepineCaseVWCloud(group_id=star.group_id,
+                                velocity_v=star.velocity_v,
+                                velocity_w=star.velocity_w)
+    elif abs(star.coordinate_y) == highest_coordinate:
+        yield LepineCaseUWCloud(group_id=star.group_id,
+                                velocity_u=star.velocity_u,
+                                velocity_w=star.velocity_w)
+    else:
+        yield LepineCaseUVCloud(group_id=star.group_id,
+                                velocity_u=star.velocity_u,
+                                velocity_v=star.velocity_v)
