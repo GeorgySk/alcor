3c8c395154c4722c83dd41b40629b057a7cd8e6b
Georgy Skorobogatov <skorobogatov@phystech.edu>
2017-08-22 20:55:04 +0200

    Moved Fortran compiler options to docker-compose.simulate.yml

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,23 +5,7 @@ cd test_project
 # unzipping seed files
 unzip -o forts.zip
 
-# TODO: move this to docker-compose.yml
-# More info at https://docs.docker.com/compose/environment-variables/
-ffpe_trap_list=$(echo "zero"\
-                      "overflow"\
-                      "invalid"\
-                 | tr [:space:] ,)
-
-fortran_compiler_options=$(echo "-g"\
-                                "-fbacktrace"\
-                                "-ffpe-trap=${ffpe_trap_list::-1}"\
-                                "-Wall"\
-                                "-Wextra"\
-                                "-Wconversion"\
-                                "-fbounds-check"\
-                                "-fcheck=all")
-# compiling Fortran files
-gfortran main.f -o main.e ${fortran_compiler_options}
+gfortran main.f -o main.e ${FORTRAN_COMPILER_OPTIONS}
 cd -
 
 python3.6 manage.py "$@"
