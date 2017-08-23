2ceb021399f4b5ed15f8ab8fcbfb77f1beba4f41
Georgy Skorobogatov <skorobogatov@phystech.edu>
2017-08-23 14:15:27 +0200

    Added Star.id in ForeignKey of ProcessedStarsAssociations

--- alcor/models/processed_star_association.py
+++ alcor/models/processed_star_association.py
@@ -3,15 +3,16 @@ from sqlalchemy.sql.schema import ForeignKey
 from sqlalchemy.sql.sqltypes import BigInteger
 
 from .base import Base
+from .star import Star
 
 
 class ProcessedStarsAssociation(Base):
     __tablename__ = 'processed_stars_associations'
     original_star_id = Column(BigInteger(),
-                              ForeignKey('stars.id'),
+                              ForeignKey(Star.id),
                               primary_key=True)
     processed_star_id = Column(BigInteger(),
-                               ForeignKey('stars.id'),
+                               ForeignKey(Star.id),
                                primary_key=True)
 
     def __init__(self,
