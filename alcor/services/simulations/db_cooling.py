83fa3ee0707f5380e22de7865b1236841873fccb
Georgy Skorobogatov <skorobogatov@phystech.edu>
2017-09-15 14:36:16 +0200

    Used zip for constructing dicts with metallicities as keys

--- alcor/services/simulations/db_cooling.py
+++ alcor/services/simulations/db_cooling.py
@@ -1,34 +1,37 @@
 import numpy as np
 
 ROWS_COUNT = 400
-FILES_PATHS = {1: ['05047_db_Z=0.001.trk',
-                   '05527_db_Z=0.001.trk',
-                   '059328_db_Z=0.001.trk',
-                   '062738_db_Z=0.001.trk',
-                   '06602_db_Z=0.001.trk',
-                   '069289_db_Z=0.001.trk',
-                   '08637_db_Z=0.001.trk'],
-               10: ['db_cool_0514.seq',
-                    'db_cool_0530.seq',
-                    'db_cool_0542.seq',
-                    'db_cool_0565.seq',
-                    'db_cool_0584.seq',
-                    'db_cool_0609.seq',
-                    'db_cool_0664.seq',
-                    'db_cool_0741.seq',
-                    'db_cool_0869.seq'],
-               60: ['0524db_006_sflhdiff.trk',
-                    '0570db_006_sflhdiff.trk',
-                    '0593db_006_sflhdiff.trk',
-                    '061db_006_sflhdiff.trk',
-                    '0632db_006_sflhdiff.trk',
-                    '0659db_006_sflhdiff.trk',
-                    '070db_006_sflhdiff.trk',
-                    '076db_006_sflhdiff.trk',
-                    '087db_006_sflhdiff.trk']}
-FILES_FOLDER = {1:  'db_cooling_tables/Z0001',
-                10: 'db_cooling_tables/Z001',
-                60: 'db_cooling_tables/Z006'}
+METALLICITIES_PER_THOUSAND = [1, 10, 60]
+FILES_PATHS = dict(zip(METALLICITIES_PER_THOUSAND,
+                       [['05047_db_Z=0.001.trk',
+                         '05527_db_Z=0.001.trk',
+                         '059328_db_Z=0.001.trk',
+                         '062738_db_Z=0.001.trk',
+                         '06602_db_Z=0.001.trk',
+                         '069289_db_Z=0.001.trk',
+                         '08637_db_Z=0.001.trk'],
+                        ['db_cool_0514.seq',
+                         'db_cool_0530.seq',
+                         'db_cool_0542.seq',
+                         'db_cool_0565.seq',
+                         'db_cool_0584.seq',
+                         'db_cool_0609.seq',
+                         'db_cool_0664.seq',
+                         'db_cool_0741.seq',
+                         'db_cool_0869.seq'],
+                        ['0524db_006_sflhdiff.trk',
+                         '0570db_006_sflhdiff.trk',
+                         '0593db_006_sflhdiff.trk',
+                         '061db_006_sflhdiff.trk',
+                         '0632db_006_sflhdiff.trk',
+                         '0659db_006_sflhdiff.trk',
+                         '070db_006_sflhdiff.trk',
+                         '076db_006_sflhdiff.trk',
+                         '087db_006_sflhdiff.trk']]))
+FILES_FOLDER = dict(zip(METALLICITIES_PER_THOUSAND,
+                        ['db_cooling_tables/Z0001',
+                         'db_cooling_tables/Z001',
+                         'db_cooling_tables/Z006']))
 MASSES = [np.array([0.5047, 0.5527, 0.59328, 0.62738,
                     0.6602, 0.69289, 0.8637]),
           np.array([0.514, 0.53, 0.542, 0.565, 0.584,
@@ -40,10 +43,7 @@ PRE_WD_LIFETIMES = [np.zeros(len(FILES_PATHS[1])),
                               0.7422, 0.4431, 0.287, 0.114]),
                     np.array([11.117, 2.7004, 1.699, 1.2114, 0.9892,
                               0.7422, 0.4431, 0.287, 0.114])]
-METALLICITIES_PER_THOUSAND = [1, 10, 60]
-FILL_TYPES_BY_METALLICITIES = {1: 0,
-                               10: 1,
-                               60: 0}
+FILL_TYPES_BY_METALLICITIES = dict(zip(METALLICITIES_PER_THOUSAND, [0, 1, 0]))
 
 
 def convert_effective_temperature(value: float,
