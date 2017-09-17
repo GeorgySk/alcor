4f474841f6d8c8b2c0715019936b90b4e0c095de
Georgy Skorobogatov <skorobogatov@phystech.edu>
2017-09-17 13:53:19 +0200

    Working only with one star, creating new instead of mutating old one

--- alcor/services/simulations/coordinates.py
+++ alcor/services/simulations/coordinates.py
@@ -1,3 +1,4 @@
+import copy
 from math import (sqrt,
                   pi,
                   sin,
@@ -5,9 +6,8 @@ from math import (sqrt,
                   asin,
                   acos,
                   atan)
-from typing import List
 
-from alcor.models.star import Star
+from alcor.models import Star
 
 
 PC_PER_KPC = 1e3
@@ -18,89 +18,106 @@ PC_PER_KPC = 1e3
 # theta - see BK angle at the link above
 # kappa - km/sec in one au/year:
 # transverse_velocity = KAPPA * proper_motion * distance
-def calculate_coordinates(stars: List[Star],
-                          solar_galactocentric_distance: float,
-                          ngp_declination: float = 0.478,
-                          theta: float = 2.147,
-                          ngp_right_ascension: float = 3.35,
-                          kappa: float = 4.74
-                          ) -> None:
-    for star in stars:
-        distance_plane_projection = (opposite_triangle_side(
-            solar_galactocentric_distance,
-            star.r_cylindric_coordinate,
-            star.th_cylindric_coordinate))
-        star.distance = sqrt(distance_plane_projection ** 2
-                             + star.z_coordinate ** 2)
+def modify(star: Star,
+           solar_galactocentric_distance: float,
+           ngp_declination: float = 0.478,
+           theta: float = 2.147,
+           ngp_right_ascension: float = 3.35,
+           kappa: float = 4.74
+           ) -> Star:
+    new_star = copy.deepcopy(star)
 
-        # TODO: implement function
-        star.galactic_longitude = acos((solar_galactocentric_distance ** 2
-                                        + distance_plane_projection ** 2
-                                        - star.r_cylindric_coordinate ** 2)
-                                       / (2. * distance_plane_projection
-                                          * solar_galactocentric_distance))
-        # Unfolding from 0-180 to 0-360
-        if star.th_cylindric_coordinate > pi:
-            star.galactic_longitude = 2. * pi - star.galactic_longitude
+    r_cylindrical_coordinate = new_star.r_cylindric_coordinate
+    th_cylindrical_coordinate = new_star.th_cylindric_coordinate
+    z_coordinate = new_star.z_coordinate
+    u_velocity = new_star.u_velocity
+    v_velocity = new_star.v_velocity
+    w_velocity = new_star.w_velocity
 
-        # TODO: or use arctan2
-        star.galactic_latitude = atan(abs(star.z_coordinate
-                                          / distance_plane_projection))
-        if star.z_coordinate < 0.:
-            star.galactic_latitude = -star.galactic_latitude
+    distance_plane_projection = (opposite_triangle_side(
+        solar_galactocentric_distance,
+        r_cylindrical_coordinate,
+        th_cylindrical_coordinate))
+    distance = sqrt(distance_plane_projection ** 2 + z_coordinate ** 2)
 
-        sin_longitude = sin(star.galactic_longitude)
-        cos_longitude = cos(star.galactic_longitude)
-        sin_latitude = sin(star.galactic_latitude)
-        cos_latitude = cos(star.galactic_latitude)
+    # TODO: implement function
+    galactic_longitude = acos((solar_galactocentric_distance ** 2
+                               + distance_plane_projection ** 2
+                               - r_cylindrical_coordinate ** 2)
+                              / (2. * distance_plane_projection
+                                 * solar_galactocentric_distance))
+    # Unfolding from 0-180 to 0-360
+    if th_cylindrical_coordinate > pi:
+        galactic_longitude = 2. * pi - galactic_longitude
 
-        velocity_by_prop_motion = 1. / (kappa * star.distance * PC_PER_KPC)
+    # TODO: or use arctan2
+    galactic_latitude = atan(abs(z_coordinate / distance_plane_projection))
+    # TODO: use sign
+    if z_coordinate < 0.:
+        galactic_latitude = -galactic_latitude
 
-        # TODO: find out if we need to divide by `cos_latitude`
-        star.proper_motion_component_l = velocity_by_prop_motion * (
-            -star.u_velocity * sin_longitude
-            + star.v_velocity * cos_longitude) / cos_latitude
-        star.proper_motion_component_b = velocity_by_prop_motion * (
-            -star.u_velocity * cos_longitude * sin_latitude
-            - star.v_velocity * sin_latitude * sin_longitude
-            + star.w_velocity * cos_latitude)
-        star.radial_velocity = (
-            (cos_latitude * cos_longitude * star.u_velocity)
-            + (cos_latitude * sin_latitude * star.v_velocity)
-            + (sin_latitude * star.w_velocity))
+    sin_longitude = sin(galactic_longitude)
+    cos_longitude = cos(galactic_longitude)
+    sin_latitude = sin(galactic_latitude)
+    cos_latitude = cos(galactic_latitude)
 
-        # TODO: find out if we need to multiply l-component by cos(b)
-        star.proper_motion = sqrt(star.proper_motion_component_l ** 2
-                                  + star.proper_motion_component_b ** 2)
+    velocity_by_prop_motion = 1. / (kappa * distance * PC_PER_KPC)
 
-        star.declination = (asin(sin(ngp_declination) * sin_latitude
-                                 + cos(ngp_declination) * cos_latitude
-                                   * cos(theta - star.galactic_longitude)))
+    # TODO: find out if we need to divide by `cos_latitude`
+    proper_motion_in_longitude = (velocity_by_prop_motion / cos_latitude) * (
+        -u_velocity * sin_longitude + v_velocity * cos_longitude)
+    proper_motion_in_latitude = velocity_by_prop_motion * (
+        -u_velocity * cos_longitude * sin_latitude
+        - v_velocity * sin_latitude * sin_longitude
+        + w_velocity * cos_latitude)
+    radial_velocity = (
+        u_velocity * cos_latitude * cos_longitude
+        + v_velocity * cos_latitude * sin_latitude
+        + w_velocity * sin_latitude)
 
-        # TODO: give better names
-        # These variables are for conversion from galactic to equatorial
-        # coordinates. More info at the link above
-        xs = ((cos_latitude * sin(theta - star.galactic_longitude))
-              / cos(star.declination))
-        xc = ((cos(ngp_declination) * sin_latitude
-               - sin(ngp_declination) * cos_latitude
-                 * cos(theta - star.galactic_longitude))
-              / cos(star.declination))
+    # TODO: find out if we need to multiply l-component by cos(b)
+    proper_motion = sqrt(proper_motion_in_longitude ** 2
+                         + proper_motion_in_latitude ** 2)
 
-        # TODO: find out what is going on here
-        if xs >= 0.:
-            if xc >= 0.:
-                star.right_ascension = asin(xs) + ngp_right_ascension
-            else:
-                star.right_ascension = acos(xc) + ngp_right_ascension
+    declination = (asin(sin(ngp_declination) * sin_latitude
+                        + cos(ngp_declination) * cos_latitude
+                          * cos(theta - galactic_longitude)))
+
+    # TODO: give better names
+    # These variables are for conversion from galactic to equatorial
+    # coordinates. More info at the link above
+    xs = cos_latitude * sin(theta - galactic_longitude) / cos(declination)
+    xc = ((cos(ngp_declination) * sin_latitude
+           - sin(ngp_declination) * cos_latitude
+             * cos(theta - galactic_longitude))
+          / cos(declination))
+
+    # TODO: find out what is going on here
+    if xs >= 0.:
+        if xc >= 0.:
+            right_ascension = asin(xs) + ngp_right_ascension
         else:
-            if xc >= 0.:
-                star.right_ascension = 2. * pi + asin(xs) + ngp_right_ascension
-            else:
-                star.right_ascension = pi - asin(xs) + ngp_right_ascension
-                
-        if star.right_ascension > 2. * pi:
-            star.right_ascension -= 2. * pi
+            right_ascension = acos(xc) + ngp_right_ascension
+    else:
+        if xc >= 0.:
+            right_ascension = 2. * pi + asin(xs) + ngp_right_ascension
+        else:
+            right_ascension = pi - asin(xs) + ngp_right_ascension
+
+    if right_ascension > 2. * pi:
+        right_ascension -= 2. * pi
+
+    new_star.distance = distance
+    new_star.galactic_longitude = galactic_longitude
+    new_star.galactic_latitude = galactic_latitude
+    new_star.proper_motion_component_l = proper_motion_in_longitude
+    new_star.proper_motion_component_b = proper_motion_in_latitude
+    new_star.radial_velocity = radial_velocity
+    new_star.proper_motion = proper_motion
+    new_star.declination = declination
+    new_star.right_ascension = right_ascension
+
+    return new_star
 
 
 def opposite_triangle_side(adjacent: float,
