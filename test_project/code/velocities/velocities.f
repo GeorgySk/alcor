757ecc1a9fca231b175f79002ea76ba5747550ca
Georgy Skorobogatov <skorobogatov@phystech.edu>
2017-09-18 14:38:59 +0200

    Fortran: added halo stars velocities

--- test_project/code/velocities/velocities.f
+++ test_project/code/velocities/velocities.f
@@ -12,7 +12,14 @@ C         TODO: this is numberOfStars, take it up as const
 C         A, B: Oort constants in km/sKpc (values by Kerr and 
 C              Lynden-Bell 1986)
           real, parameter :: oort_const_A = 14.4,
-     &                       oort_const_B = -12.8
+     &                       oort_const_B = -12.8,
+     &                       ro = 10.5,
+     &                       l = 5.5,
+     &                       sigo = 80.0,
+     &                       sigm = 145.0,
+     &                       pi = 4.0 * atan(1.0),
+     &                       vc = 220.0,
+     &                       gamma = 3.4
           double precision, parameter :: solar_galactocentric_distance 
      &                                   = 8.5d0
           integer :: iseed,
@@ -34,11 +41,33 @@ C         Heliocentric velocities, B3 system
      &            uu(MAX_STARS_COUNT),
      &            vv(MAX_STARS_COUNT),
      &            ww(MAX_STARS_COUNT),
-     &            velocities_dispersions(3)
+     &            velocities_dispersions(3),
+     &            stellar_galactocentric_distance,
+     &            xx,
+     &            sigo2,
+     &            sigm2,
+     &            sigr2,
+     &            dsigr2dr,
+     &            sigt2,
+     &            sigr,
+     &            sigt,
+     &            vr,
+     &            vth,
+     &            vph,
+     &            delta,
+     &            sindel,
+     &            cosdel,
+     &            vx,
+     &            vy,
+     &            vz,
+     &            sini,
+     &            cosi
           character(len = 6) :: geometry
           double precision :: coordinate_R(MAX_STARS_COUNT),
      &                        coordinate_Theta(MAX_STARS_COUNT),
-     &                        coordinate_Zcylindr(MAX_STARS_COUNT)
+     &                        coordinate_Zcylindr(MAX_STARS_COUNT),
+     &                        lgac(MAX_STARS_COUNT),
+     &                        bgac(MAX_STARS_COUNT)
           
           common /vel/ uu, vv, ww
           common /coorcil/ coordinate_R,
@@ -47,6 +76,7 @@ C         Heliocentric velocities, B3 system
           common /index/ flagOfWD,
      &                   numberOfWDs,
      &                   disk_belonging
+          common /lb/ lgac, bgac
     
 C         Peculiar solar velocities, values from Anguiano et al. 2017
 C         TODO: find out why signs are different from values in Python
@@ -77,7 +107,7 @@ C             TODO: find these constants and make variables for them
                   velocities_dispersions(1) = 50.0
                   velocities_dispersions(2) = 56.0
                   velocities_dispersions(3) = 34.0
-              else
+              else if (disk_belonging(i) /= 3) then
                   write(6, *) "Error in velh:"
                   write(6, *) "    Number of WD's =", numberOfWDs
                   write(6, *) "    index =", i
@@ -85,29 +115,76 @@ C             TODO: find these constants and make variables for them
                   stop
               end if
             
-C             Calling function of gaussian distribution
-C             TODO: find out the meaning of uop
-              uop = uom(coordinate_R(i),
-     &                  coordinate_Theta(i),
-     &                  oort_const_A, 
-     &                  oort_const_B,
-     &                  solar_galactocentric_distance,
-     &                  peculiar_solar_velocity_u)
-              uu(i) = velocities_dispersions(1) * gasdev(iseed) + uop
-C             TODO: find out the meaning of vop
-              vop = vom(coordinate_R(i),
-     &                  coordinate_Theta(i),
-     &                  oort_const_A, 
-     &                  oort_const_B,
-     &                  solar_galactocentric_distance,
-     &                  peculiar_solar_velocity_v)
-C             TODO: find out what this 120.0 is
-              vv(i) = velocities_dispersions(2) * gasdev(iseed) + vop 
-     &                - velocities_dispersions(1) 
-     &                  * velocities_dispersions(1)
-     &                  / 120.0
-              ww(i) = velocities_dispersions(3) * gasdev(iseed) 
-     &                + peculiar_solar_velocity_w
+C             TODO: find out what is going on here (see article 
+C             Simulating Gaia performances on white dwarfs by Torres)
+              if (disk_belonging(i) == 3) then
+C                 Radial and tangential dispersions
+                  stellar_galactocentric_distance = real(sqrt(
+     &                coordinate_R(i) ** 2 
+     &                + coordinate_Zcylindr(i) ** 2))
+                  xx = (stellar_galactocentric_distance - ro) / l
+                  sigo2 = sigo * sigo
+                  sigm2 = sigm * sigm
+                  sigr2 = sigo2 + sigm2 * (0.5 - (atan(xx) / pi))
+                  dsigr2dr = -(1.0 / l) * sigm2 / ((1.0 + xx * xx) * pi)
+                  sigt2 = 0.5 * vc * vc + (1.0 - gamma / 2.0) * sigr2 
+     &                    + (stellar_galactocentric_distance / 2.0) 
+     &                      * dsigr2dr
+                  sigr = sqrt(sigr2)
+                  sigt = sqrt(sigt2)
+                  sigr = vc / sqrt(2.0)
+                  sigt = vc / sqrt(2.0)
+
+C                 Spherical coordinates
+                  vr = sigr * gasdev(iseed)
+                  vth = sigt * gasdev(iseed)
+                  vph = sigt * gasdev(iseed)
+
+C                 Cartesian coordinates
+                  delta = real(pi - lgac(i) - coordinate_Theta(i))
+                  sindel = sin(delta)
+                  cosdel = cos(delta)
+                  vx = -cosdel * vr + sindel * vph
+                  vy = sindel * vr + cosdel * vph
+                  vz = vth
+
+                  sini = sin(real(lgac(i)))
+                  cosi = cos(real(lgac(i)))
+
+                  uu(i) = vz * cosi + vx * sini
+                  vv(i) = -vz * sini + vx * cosi
+                  ww(i) = vy
+
+                  vv(i) = vv(i) - vc
+
+                  uu(i) = uu(i) + peculiar_solar_velocity_u
+                  vv(i) = vv(i) + peculiar_solar_velocity_v
+                  ww(i) = ww(i) + peculiar_solar_velocity_w
+              else
+C                 Calling function of gaussian distribution
+C                 TODO: find out the meaning of uop
+                  uop = uom(coordinate_R(i),
+     &                      coordinate_Theta(i),
+     &                      oort_const_A, 
+     &                      oort_const_B,
+     &                      solar_galactocentric_distance,
+     &                      peculiar_solar_velocity_u)
+                  uu(i) = velocities_dispersions(1) * gasdev(iseed) +uop
+C                 TODO: find out the meaning of vop
+                  vop = vom(coordinate_R(i),
+     &                      coordinate_Theta(i),
+     &                      oort_const_A, 
+     &                      oort_const_B,
+     &                      solar_galactocentric_distance,
+     &                      peculiar_solar_velocity_v)
+C                 TODO: find out what this 120.0 is
+                  vv(i) = velocities_dispersions(2) * gasdev(iseed) +vop 
+     &                    - velocities_dispersions(1) 
+     &                      * velocities_dispersions(1)
+     &                      / 120.0
+                  ww(i) = velocities_dispersions(3) * gasdev(iseed) 
+     &                    + peculiar_solar_velocity_w
+              end if
           end do
       end subroutine
 
