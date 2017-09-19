a69ba287f15da7390c98d1461f76642c62347e48
Georgy Skorobogatov <skorobogatov@phystech.edu>
2017-09-19 16:29:08 +0200

    Implemented test for da colors tables

--- fortran_tests/tables/da_color.f90
+++ fortran_tests/tables/da_color.f90
@@ -1,46 +1,51 @@
-      subroutine color(table)
-C         Reading colors by Rene and interpolating according to the 
-C         vector of reference time
-          use external_types
-          implicit none
+subroutine color(initlink, ncol, nrow, ntrk, mass, luminosity, color_u, &
+                 color_b, color_v, color_r, color_i)
+    implicit none
 
-          real, parameter :: VTANMIN = 30
-          TYPE(FileGroupInfo) :: table
-          integer :: i, k
-          real :: a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, 
-     &            a13, a14, a15, a16, a17, a18, a19, a20, a21, a22, a23,
-     &            a24, a25, a26, a27, a28
+    integer, intent(in) :: initLink
+    integer, intent(in) :: ncol
+    integer, intent(in) :: nrow
+    integer, intent(inout) :: ntrk(ncol)
+    real, intent(inout) :: mass(ncol)
+    real, intent(inout) :: luminosity(ncol, nrow)
+    real, intent(inout) :: color_U(ncol, nrow)
+    real, intent(inout) :: color_B(ncol, nrow)
+    real, intent(inout) :: color_V(ncol, nrow)
+    real, intent(inout) :: color_R(ncol, nrow)
+    real, intent(inout) :: color_I(ncol, nrow)
+    integer :: i, k
+    real :: a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, &
+            a13, a14, a15, a16, a17, a18, a19, a20, a21, a22, a23, &
+            a24, a25, a26, a27, a28
 
-C         Read masses
-          table%mass(1) = 0.524
-          table%mass(2) = 0.570
-          table%mass(3) = 0.593
-          table%mass(4) = 0.610
-          table%mass(5) = 0.632
-          table%mass(6) = 0.659
-          table%mass(7) = 0.705
-          table%mass(8) = 0.767
-          table%mass(9) = 0.837
-          table%mass(10) = 0.877
-          
-C         Read values from files
-          do k = 1, table%ncol
-              do i = 1, table%nrow
-                  read(table%initLink + k, *, end=15) a1, a2, a3, a4, 
-     &                                                a5, a6, a7, a8, 
-     &                                                a9, a10, a11, a12,
-     &                                                a13, a14, a15, 
-     &                                                a16, a17, a18, 
-     &                                                a19, a20, a21, 
-     &                                                a22, a23, a24, 
-     &                                                a25, a26, a27, a28
-                  table%luminosity(k,i) = a3
-                  table%color_U(k,i) = a24
-                  table%color_B(k,i) = a25
-                  table%color_V(k,i) = a26
-                  table%color_R(k,i) = a27
-                  table%color_I(k,i) = a28
-              end do
-15            table%ntrk(k) = i - 1
-          end do
-      end subroutine
+    mass(1) = 0.524
+    mass(2) = 0.570
+    mass(3) = 0.593
+    mass(4) = 0.610
+    mass(5) = 0.632
+    mass(6) = 0.659
+    mass(7) = 0.705
+    mass(8) = 0.767
+    mass(9) = 0.837
+    mass(10) = 0.877
+
+    do k = 1, ncol
+        do i = 1, nrow
+            read(initLink + k, *, end=15) a1, a2, a3, a4, &
+                                          a5, a6, a7, a8,  &
+                                          a9, a10, a11, a12, &
+                                          a13, a14, a15, &
+                                          a16, a17, a18, &
+                                          a19, a20, a21, &
+                                          a22, a23, a24,  &
+                                          a25, a26, a27, a28
+                  luminosity(k,i) = a3
+                  color_U(k,i) = a24
+                  color_B(k,i) = a25
+                  color_V(k,i) = a26
+                  color_R(k,i) = a27
+                  color_I(k,i) = a28
+        end do
+15      ntrk(k) = i - 1
+    end do
+end subroutine
--- fortran_tests/tables/test_table.py
+++ fortran_tests/tables/test_table.py
@@ -1,6 +1,7 @@
 from math import isclose
 from typing import (Union,
                     Callable,
+                    Dict,
                     Tuple,
                     List)
 
@@ -11,11 +12,12 @@ import numpy as np
 
 import da_cooling
 import db_cooling
+import da_color
 
 
 def test_table() -> None:
     da_cooling_table_by_python = table.read(table_name='da_cooling')
-    da_cooling_table_by_fortran = read_sequences_from_fortran(
+    da_cooling_table_by_fortran = read_sequences_by_metallicity_from_fortran(
         rows_count=650,
         files_counts_per_metallicity=[7, 10, 8, 8],
         fill_types=[1, 2, 3, 3],
@@ -23,11 +25,11 @@ def test_table() -> None:
         metallicities_by_thousand=[1, 10, 30, 60],
         get_from_fortran=da_cooling.incoolda)
 
-    assert values_are_close(da_cooling_table_by_python,
-                            da_cooling_table_by_fortran)
+    assert values_by_metallicity_are_close(da_cooling_table_by_python,
+                                           da_cooling_table_by_fortran)
 
     db_cooling_table_by_python = table.read(table_name='db_cooling')
-    db_cooling_table_by_fortran = read_sequences_from_fortran(
+    db_cooling_table_by_fortran = read_sequences_by_metallicity_from_fortran(
         rows_count=400,
         files_counts_per_metallicity=[7, 9, 9],
         fill_types=[1, 2, 3],
@@ -35,17 +37,87 @@ def test_table() -> None:
         metallicities_by_thousand=[1, 10, 60],
         get_from_fortran=db_cooling.incooldb)
 
-    assert values_are_close(db_cooling_table_by_python,
-                            db_cooling_table_by_fortran)
+    assert values_by_metallicity_are_close(db_cooling_table_by_python,
+                                           db_cooling_table_by_fortran)
+
+    da_color_table_by_python = table.read(table_name='da_color')
+    da_color_table_by_fortran = read_colors_from_fortran(
+        rows_count=650,
+        files_count=10,
+        fort_files_initial_unit=60,
+        get_from_fortran=da_color.color)
+
+    assert values_are_close(da_color_table_by_python,
+                            da_color_table_by_fortran)
 
 
-def read_sequences_from_fortran(rows_count: int,
-                                files_counts_per_metallicity: List[int],
-                                fill_types: List[int],
-                                fort_files_initial_units: List[int],
-                                metallicities_by_thousand: List[int],
-                                get_from_fortran: Callable,
-                                ) -> CoolingSequencesType:
+def values_are_close(x: Dict[str, np.ndarray],
+                     y: Dict[str, np.ndarray],
+                     rel_tol: float = 1E-4) -> bool:
+    for key, x_values in x.items():
+        y_values = y[key]
+        for x_value, y_value in zip(x_values, y_values):
+            if x_value.shape is ():
+                if not np.isnan(x_value) and not np.isnan(y_value):
+                    if not isclose(x_value,
+                                   y_value,
+                                   rel_tol=rel_tol):
+                        return False
+            else:
+                for (x_sub_value, y_sub_value) in zip(x_value, y_value):
+                    if (not np.isnan(x_sub_value)
+                            and not np.isnan(y_sub_value)):
+                        if not isclose(x_sub_value,
+                                       y_sub_value,
+                                       rel_tol=rel_tol):
+                            return False
+    return True
+
+
+# TODO: rename rows_count to max_rows_count
+def read_colors_from_fortran(rows_count: int,
+                             files_count: int,
+                             fort_files_initial_unit: int,
+                             get_from_fortran: Callable
+                             ) -> Dict[str, np.ndarray]:
+    rows_counts = nan_matrix(shape=files_count, dtype='i')
+    masses = nan_matrix(shape=files_count)
+    luminosities = nan_matrix(shape=(files_count, rows_count), order='F')
+    u_ubvri = nan_matrix(shape=(files_count, rows_count), order='F')
+    b_ubvri = nan_matrix(shape=(files_count, rows_count), order='F')
+    v_ubvri = nan_matrix(shape=(files_count, rows_count), order='F')
+    r_ubvri = nan_matrix(shape=(files_count, rows_count), order='F')
+    i_ubvri = nan_matrix(shape=(files_count, rows_count), order='F')
+
+    get_from_fortran(initlink=fort_files_initial_unit,
+                     ntrk=rows_counts,
+                     mass=masses,
+                     luminosity=luminosities,
+                     color_u=u_ubvri,
+                     color_b=b_ubvri,
+                     color_v=v_ubvri,
+                     color_r=r_ubvri,
+                     color_i=i_ubvri)
+
+    return dict(mass=masses,
+                luminosity=luminosities,
+                u_ubvri_absolute=u_ubvri,
+                b_ubvri_absolute=b_ubvri,
+                v_ubvri_absolute=v_ubvri,
+                r_ubvri_absolute=r_ubvri,
+                i_ubvri_absolute=i_ubvri,
+                rows_counts=rows_counts)
+
+
+# TODO: rename rows_count to max_rows_count
+def read_sequences_by_metallicity_from_fortran(
+        rows_count: int,
+        files_counts_per_metallicity: List[int],
+        fill_types: List[int],
+        fort_files_initial_units: List[int],
+        metallicities_by_thousand: List[int],
+        get_from_fortran: Callable,
+        ) -> CoolingSequencesType:
     metallicities_count = len(files_counts_per_metallicity)
     rows_counts = [nan_matrix(shape=files_count, dtype='i')
                    for files_count in files_counts_per_metallicity]
@@ -99,9 +171,9 @@ def nan_matrix(shape: Union[int, Tuple[int, ...]],
                    order=order)
 
 
-def values_are_close(x: CoolingSequencesType,
-                     y: CoolingSequencesType,
-                     rel_tol: float = 1E-4) -> bool:
+def values_by_metallicity_are_close(x: CoolingSequencesType,
+                                    y: CoolingSequencesType,
+                                    rel_tol: float = 1E-4) -> bool:
     for metallicity, x_sequences in x.items():
         y_sequences = y[metallicity]
         for key, x_values in x_sequences.items():
