ec6d2689ccacb0eb72ce5a6258831b35b4d6560f
Georgy Skorobogatov <skorobogatov@phystech.edu>
2017-09-28 17:09:45 +0200

    Fix: used abs magnitudes instead of apparent when filtering for restricted sample

--- alcor/services/stars_group/elimination.py
+++ alcor/services/stars_group/elimination.py
@@ -52,23 +52,30 @@ def check(star: Star,
                                  - 1.584 * float(star.r_abs_magnitude
                                                  - star.i_abs_magnitude)
                                  + 0.525)
+        g_apparent_magnitude = apparent_magnitude(
+                g_ugriz_abs_magnitude,
+                distance_kpc=float(star.distance))
+        z_apparent_magnitude = apparent_magnitude(
+                z_ugriz_abs_magnitude,
+                distance_kpc=float(star.distance))
         # TODO: find out the meaning and check if the last 5 is correct
-        hrm = g_ugriz_abs_magnitude + float(
+        hrm = g_apparent_magnitude + float(
                 5. * log10(star.proper_motion) + 5.)
-        v_apparent_magnitude = float(star.v_abs_magnitude) - 5. + 5. * (
-            log10(float(star.distance)) + 3.)
+        v_apparent_magnitude = apparent_magnitude(
+                float(star.v_abs_magnitude),
+                distance_kpc=float(star.distance))
 
         if star.proper_motion < min_proper_motion:
             eliminations_counter['by_proper_motion'] += 1
             return True
         # TODO: find out the meaning of the following constants
-        elif (g_ugriz_abs_magnitude - z_ugriz_abs_magnitude < -0.33
+        elif (g_apparent_magnitude - z_apparent_magnitude < -0.33
               and hrm < 14.):
             eliminations_counter['by_reduced_proper_motion'] += 1
             return True
         # TODO: find out the meaning of the following constants
-        elif (hrm < 3.559 * (g_ugriz_abs_magnitude - z_ugriz_abs_magnitude)
-              + 15.17):
+        elif (hrm < 3.559 * (g_apparent_magnitude - z_apparent_magnitude)
+                    + 15.17):
             eliminations_counter['by_reduced_proper_motion'] += 1
             return True
         # TODO: find out the meaning of the following constant
@@ -76,3 +83,11 @@ def check(star: Star,
             eliminations_counter['by_apparent_magnitude'] += 1
             return True
     return False
+
+
+def apparent_magnitude(abs_magnitude: float,
+                       distance_kpc: float
+                       ) -> float:
+    # More info at (2nd formula, + 3.0 because the distance is in kpc):
+    # https://en.wikipedia.org/wiki/Absolute_magnitude#Apparent_magnitude
+    return abs_magnitude - 5. + 5. * (log10(distance_kpc) + 3.)
