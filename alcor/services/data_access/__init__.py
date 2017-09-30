672abc24853e9698493ee811e84946a0186ce571
Georgy Skorobogatov <skorobogatov@phystech.edu>
2017-09-30 23:49:20 +0200

    Removed traces of unprocessed groups

--- alcor/services/data_access/__init__.py
+++ alcor/services/data_access/__init__.py
@@ -1,6 +1,5 @@
 from .service import (fetch_all,
                       fetch_random,
                       fetch_group_stars,
-                      fetch_unprocessed_groups,
                       fetch_group_by_id,
                       fetch_last_groups)
