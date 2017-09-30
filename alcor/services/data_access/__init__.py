86e7831924c62caad81bec0f642fb24794a1c5e9
Georgy Skorobogatov <skorobogatov@phystech.edu>
2017-10-01 00:21:35 +0200

    Removed fetch_random

--- alcor/services/data_access/__init__.py
+++ alcor/services/data_access/__init__.py
@@ -1,5 +1,4 @@
 from .service import (fetch_all,
-                      fetch_random,
                       fetch_group_stars,
                       fetch_random_group_stars,
                       fetch_group_by_id,
