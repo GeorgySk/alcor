60e6fe13f61212b77ed0b2a32772295285b377b4
Georgy Skorobogatov <skorobogatov@phystech.edu>
2017-10-03 16:14:21 +0200

    Luminosity function plotting redone

--- alcor/services/plots/luminosity_function.py
+++ alcor/services/plots/luminosity_function.py
@@ -1,8 +1,7 @@
-from math import (log10,
-                  sqrt)
-from typing import (Iterable,
-                    Iterator,
+from typing import (Union,
+                    Callable,
                     Tuple,
+                    Set,
                     List)
 
 import matplotlib
@@ -10,55 +9,42 @@ import matplotlib
 # More info at
 # http://matplotlib.org/faq/usage_faq.html#what-is-a-backend for details
 # TODO: use this: https://stackoverflow.com/a/37605654/7851470
-from alcor.types import StarsBinsType
-
 matplotlib.use('Agg')
 from matplotlib import pyplot as plt
 import numpy as np
+import pandas as pd
+from pandas.core.frame import DataFrame
 
 from alcor.models import Star
-from alcor.models.luminosity_function import Point
-from alcor.services.common import (FORTY_PARSEC_NORTHERN_HEMISPHERE_VOLUME,
-                                   stars_packer,
-                                   star_bolometric_indexer,
-                                   bolometric_indexer)
 
-MIN_BOLOMETRIC_MAGNITUDE = 6.
-STARS_BIN_SIZE = 0.5
-MAX_BOLOMETRIC_MAGNITUDE = 21.
-# More info at (The white dwarf population within 40 pc of the Sun,
-# Torres et al., 2016)
-SMALLEST_ERROR_BARS_BINS_INDEXES = {15, 16, 17}
-OBSERVATIONAL_DATA_TRUSTED_BINS_OBJECT_COUNT = 220
-# Observational LF of 40pc sample from Althaus
-OBSERVATIONAL_AVG_BIN_MAGNITUDES = np.arange(7.75, 17.25, 0.5)
-OBSERVATIONAL_STARS_COUNTS = [3, 4, 5, 7, 12, 17, 17, 12, 20, 19, 37, 42, 52,
-                              72, 96, 62, 20, 3, 1]
-OBSERVATIONAL_STARS_COUNTS_LOGARITHMS = [
-    log10(stars_count / FORTY_PARSEC_NORTHERN_HEMISPHERE_VOLUME)
-    for stars_count in OBSERVATIONAL_STARS_COUNTS]
-OBSERVATIONAL_UPPER_ERRORBARS = [
-    log10((stars_count + sqrt(stars_count))
-          / FORTY_PARSEC_NORTHERN_HEMISPHERE_VOLUME)
-    - log10(stars_count / FORTY_PARSEC_NORTHERN_HEMISPHERE_VOLUME)
-    for stars_count in OBSERVATIONAL_STARS_COUNTS]
-EPSILON = 1e-6
-OBSERVATIONAL_LOWER_ERRORBARS = [
-    log10(stars_count / FORTY_PARSEC_NORTHERN_HEMISPHERE_VOLUME)
-    - log10((stars_count - sqrt(stars_count) + EPSILON)
-            / FORTY_PARSEC_NORTHERN_HEMISPHERE_VOLUME)
-    for stars_count in OBSERVATIONAL_STARS_COUNTS]
-OBSERVATIONAL_ASYMMETRIC_ERRORBARS = [OBSERVATIONAL_LOWER_ERRORBARS,
-                                      OBSERVATIONAL_UPPER_ERRORBARS]
-bolometric_index = bolometric_indexer(min_magnitude=MIN_BOLOMETRIC_MAGNITUDE,
-                                      stars_bin_size=STARS_BIN_SIZE)
-STARS_BINS_COUNT = bolometric_index(MAX_BOLOMETRIC_MAGNITUDE)
-star_bolometric_index = star_bolometric_indexer(bolometric_index)
-pack_stars = stars_packer(stars_bins_count=STARS_BINS_COUNT,
-                          star_bolometric_index=star_bolometric_index)
+OBSERVATIONAL_STARS_COUNTS = np.array(
+        [3, 4, 5, 7, 12, 17, 17, 12, 20, 19, 37, 42, 52, 72, 96, 62, 20, 3, 1])
+
+# TODO: replace by # 2 * pi * radius ** 3 / 3
+FORTY_PARSEC_NORTHERN_HEMISPHERE_VOLUME = 134041.29
 
 
+def bolometric_indexer(*,
+                       min_magnitude: float,
+                       stars_bin_size: float) -> Callable[[np.ndarray],
+                                                          np.ndarray]:
+    def bolometric_index(magnitude: np.ndarray) -> np.ndarray:
+        magnitude_amplitude = magnitude - min_magnitude
+        return np.floor(magnitude_amplitude / stars_bin_size).astype(int)
+
+    return bolometric_index
+
+
+# TODO: there is no need to have stars with all the fields
 def plot(stars: List[Star],
+         min_bolometric_magnitude: float = 6.,
+         max_bolometric_magnitude: float = 21.,
+         bin_size: float = 0.5,
+         min_observed_magnitude: float = 7.75,
+         observed_stars_counts: np.ndarray = OBSERVATIONAL_STARS_COUNTS,
+         # We choose these bins because they have many objects and don't lie in
+         # problematic regions
+         trusted_bins: Set = {15, 16, 17},
          filename: str = 'luminosity_function.ps',
          figure_size: Tuple[float, float] = (7, 7),
          ratio: float = 10 / 13,
@@ -70,49 +56,69 @@ def plot(stars: List[Star],
          marker: str = 's',
          capsize: float = 5,
          observational_line_color: str = 'r') -> None:
-    stars_bins = pack_stars(stars)
-    normalization_factor = stars_bins_normalization_factor(stars_bins)
-    stars_counts = map(len, stars_bins)
-    points = list(graph_points(stars_counts=stars_counts,
-                               normalization_factor=normalization_factor))
+    bolometric_index = bolometric_indexer(
+        min_magnitude=min_bolometric_magnitude,
+        stars_bin_size=bin_size)
 
-    avg_bin_magnitudes = [graph_point.avg_bin_magnitude
-                          for graph_point in points]
-    stars_count_logarithms = [graph_point.stars_count_logarithm
-                              for graph_point in points]
+    stars_bins_count = np.asscalar(bolometric_index(max_bolometric_magnitude))
+
+    # Aligning observed stars counts with the scale defined by min and max
+    # bolometric magnitudes
+    initial_index = np.asscalar(bolometric_index(min_observed_magnitude))
+    observed_stars_counts = np.insert(arr=observed_stars_counts,
+                                      obj=0,
+                                      values=np.zeros(shape=initial_index,
+                                                      dtype=int))
+    observed_stars_counts = np.append(
+            arr=observed_stars_counts,
+            values=np.zeros(
+                    shape=stars_bins_count - observed_stars_counts.size,
+                    dtype=int))
+
+    observational_luminosity_function = luminosity_function(
+            max_bolometric_magnitude=max_bolometric_magnitude,
+            min_bolometric_magnitude=min_bolometric_magnitude,
+            bin_size=bin_size,
+            stars_bins_count=stars_bins_count,
+            stars_counts=observed_stars_counts)
+
+    magnitudes = np.array([star.bolometric_magnitude
+                           for star in stars])
+    bins_indexes = pd.Series(bolometric_index(magnitudes))
+
+    bins_counters = np.zeros(shape=stars_bins_count,
+                             dtype=int)
+    # More info on np.ndarray iterations at https://goo.gl/jcqJFY
+    # More info on indexation at https://goo.gl/uH7dUx
+    for index, bin_counter in zip(np.ndindex(bins_counters.shape),
+                                  np.nditer(op=bins_counters,
+                                            op_flags=['readwrite'])):
+        bin_counter[...] = bins_indexes[bins_indexes == index].count()
 
     # As simulated number of white dwarfs (WDs) can be much greater than the
     # number of WDs for which observational luminosity function (LF) is
     # plotted, we normalize the LF based on synthetic(simulated) WDs
     # to the observational one (move to the same location on the plot).
-    # We do it in 'processing' module. TODO: move it here
-    # Another thing is that error bars become very small for synthetic LF
-    # and here we 1) get number of stars for which we would have the
-    # corresponding normalized logN and 2) calculate new not squeezed error
-    # bars corresponding to this number of stars
-    normalized_stars_counts = [
-        FORTY_PARSEC_NORTHERN_HEMISPHERE_VOLUME * 10. ** stars_count_logarithm
-        for stars_count_logarithm in stars_count_logarithms]
-    upper_errorbars = [
-        log10((stars_count + sqrt(stars_count))
-              / FORTY_PARSEC_NORTHERN_HEMISPHERE_VOLUME)
-        - log10(stars_count / FORTY_PARSEC_NORTHERN_HEMISPHERE_VOLUME)
-        for stars_count in normalized_stars_counts]
+    actual_stars_counts = pd.Series(bins_counters)
 
-    lower_errorbars = []
-    for stars_count in normalized_stars_counts:
-        try:
-            lower_errorbars.append(
-                    log10(stars_count
-                          / FORTY_PARSEC_NORTHERN_HEMISPHERE_VOLUME)
-                    - log10((stars_count - sqrt(stars_count) + EPSILON)
-                            / FORTY_PARSEC_NORTHERN_HEMISPHERE_VOLUME))
-        except ValueError:
-            # Some number so that errorbar would go below the plot
-            lower_errorbars.append(5.0)
+    observed_stars_counts = pd.Series(observed_stars_counts)
+    trusted_bins_observed_stars_count = (
+        observed_stars_counts[
+            observed_stars_counts.index.isin(trusted_bins)].sum())
+    trusted_bins_simulated_stars_count = (
+        actual_stars_counts[
+            actual_stars_counts.index.isin(trusted_bins)].sum())
 
-    asymmetric_errorbars = [lower_errorbars,
-                            upper_errorbars]
+    normalized_stars_counts = (actual_stars_counts
+                               * trusted_bins_observed_stars_count
+                               / trusted_bins_simulated_stars_count)
+
+    synthetic_luminosity_function = luminosity_function(
+            max_bolometric_magnitude=max_bolometric_magnitude,
+            min_bolometric_magnitude=min_bolometric_magnitude,
+            bin_size=bin_size,
+            stars_bins_count=stars_bins_count,
+            stars_counts=normalized_stars_counts)
 
     figure, subplot = plt.subplots(figsize=figure_size)
 
@@ -121,17 +127,20 @@ def plot(stars: List[Star],
                 xlim=xlimits,
                 ylim=ylimits)
 
-    subplot.errorbar(x=avg_bin_magnitudes,
-                     y=stars_count_logarithms,
-                     yerr=asymmetric_errorbars,
+    subplot.errorbar(x=synthetic_luminosity_function['magnitude'],
+                     y=synthetic_luminosity_function['log_stars_count'],
+                     yerr=[synthetic_luminosity_function['lower_errorbar'],
+                           synthetic_luminosity_function['upper_errorbar']],
                      marker=marker,
                      color=line_color,
                      capsize=capsize,
                      zorder=2)
 
-    subplot.errorbar(x=OBSERVATIONAL_AVG_BIN_MAGNITUDES,
-                     y=OBSERVATIONAL_STARS_COUNTS_LOGARITHMS,
-                     yerr=OBSERVATIONAL_ASYMMETRIC_ERRORBARS,
+    subplot.errorbar(x=observational_luminosity_function['magnitude'],
+                     y=observational_luminosity_function['log_stars_count'],
+                     yerr=[
+                         observational_luminosity_function['lower_errorbar'],
+                         observational_luminosity_function['upper_errorbar']],
                      marker=marker,
                      color=observational_line_color,
                      capsize=capsize,
@@ -147,60 +156,44 @@ def plot(stars: List[Star],
     plt.savefig(filename)
 
 
-def graph_points(*,
-                 stars_counts: Iterable[int],
-                 normalization_factor: float) -> Iterator[Point]:
-    for index, stars_count in enumerate(stars_counts):
-        if not stars_count:
-            continue
-
-        avg_bin_magnitude = (MIN_BOLOMETRIC_MAGNITUDE
-                             + STARS_BIN_SIZE * (index + 0.5))
-        logarithm = to_logarithm(stars_count=stars_count,
-                                 normalization_factor=normalization_factor)
-        upper_error_bar = to_upper_error_bar(stars_count)
-        lower_error_bar = to_lower_error_bar(stars_count)
-        yield Point(avg_bin_magnitude=avg_bin_magnitude,
-                    stars_count_logarithm=logarithm,
-                    upper_error_bar=upper_error_bar,
-                    lower_error_bar=lower_error_bar)
+def nan_array(shape: Union[int, Tuple[int, ...]]) -> np.ndarray:
+    return np.full(shape, np.nan)
 
 
-def stars_bins_normalization_factor(
-        stars_bins: StarsBinsType,
-        stars_bins_indexes: Iterable[int] = SMALLEST_ERROR_BARS_BINS_INDEXES
-) -> float:
-    smallest_error_bars_bins_lengths_sum = (
-        sum(len(stars_bins[index])
-            for index in stars_bins_indexes))
-    return (FORTY_PARSEC_NORTHERN_HEMISPHERE_VOLUME
-            * smallest_error_bars_bins_lengths_sum
-            / OBSERVATIONAL_DATA_TRUSTED_BINS_OBJECT_COUNT)
+def luminosity_function(
+        min_bolometric_magnitude: float,
+        max_bolometric_magnitude: float,
+        bin_size: float,
+        stars_bins_count: int,
+        stars_counts: np.ndarray,
+        max_errorbar_len: float = 6.
+) -> DataFrame:
+    luminosity_function_template = dict(
+            magnitude=np.arange(min_bolometric_magnitude + bin_size / 2,
+                                max_bolometric_magnitude,
+                                bin_size),
+            log_stars_count=nan_array(stars_bins_count),
+            upper_errorbar=nan_array(stars_bins_count),
+            lower_errorbar=nan_array(stars_bins_count))
 
+    res = pd.DataFrame(data=luminosity_function_template)
 
-def to_logarithm(stars_count: int,
-                 normalization_factor: float) -> float:
-    try:
-        return log10(stars_count / normalization_factor)
-    except ValueError:
-        return 0.
+    res['log_stars_count'] = np.log10(
+            stars_counts / FORTY_PARSEC_NORTHERN_HEMISPHERE_VOLUME)
+    res['upper_errorbar'] = np.log10(1. + 1. / np.sqrt(stars_counts))
+    res['lower_errorbar'] = -np.log10(1. - 1. / np.sqrt(stars_counts))
 
+    res.replace(to_replace=[np.inf, -np.inf],
+                value=np.nan,
+                inplace=True)
 
-def to_upper_error_bar(stars_count: int) -> float:
-    if stars_count == 0:
-        return 0.
-    else:
-        return (log10((stars_count + sqrt(stars_count))
-                      / FORTY_PARSEC_NORTHERN_HEMISPHERE_VOLUME)
-                - log10(stars_count / FORTY_PARSEC_NORTHERN_HEMISPHERE_VOLUME))
+    # Replacing erroneous NaN values for errorbars
+    missing_values_rows = res.loc[
+        (res.isnull().any(axis=1))
+        & (res['log_stars_count'].notnull()), :].copy()
+    missing_values_rows.fillna(value=max_errorbar_len,
+                               inplace=True)
+    res.loc[(res.isnull().any(axis=1))
+            & (res['log_stars_count'].notnull()), :] = missing_values_rows
 
-
-def to_lower_error_bar(stars_count: int) -> float:
-    if stars_count == 0:
-        return 0.
-    elif stars_count == 1:
-        return 5.  # Some number so that error bar would go below the plot
-    else:
-        return (log10(stars_count / FORTY_PARSEC_NORTHERN_HEMISPHERE_VOLUME)
-                - log10((stars_count - sqrt(stars_count))
-                        / FORTY_PARSEC_NORTHERN_HEMISPHERE_VOLUME))
+    return res
