3d87f5d3170f3436fa889cdd7a32325f9353fffc
Georgy Skorobogatov <skorobogatov@phystech.edu>
2017-10-07 17:11:49 +0200

    Removed old models

--- alcor/models/velocities_vs_magnitudes/__init__.py
+++
@@ -1,8 +0,0 @@
-from .bins import (Bin,
-                   LepineCaseUBin,
-                   LepineCaseVBin,
-                   LepineCaseWBin)
-from .clouds import (Cloud,
-                     LepineCaseUCloud,
-                     LepineCaseVCloud,
-                     LepineCaseWCloud)
