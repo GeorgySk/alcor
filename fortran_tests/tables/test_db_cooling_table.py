665da8f8b564ad7a7d633173b764b96f04327642
Georgy Skorobogatov <skorobogatov@phystech.edu>
2017-10-08 16:16:06 +0200

    Added files_counts_per_metallicity in tests from simulations module

--- fortran_tests/tables/test_db_cooling_table.py
+++ fortran_tests/tables/test_db_cooling_table.py
@@ -1,6 +1,7 @@
 from alcor.services.simulations import table
 from alcor.services.simulations.db_cooling import (ROWS_COUNT,
-                                                   METALLICITIES_PER_THOUSAND)
+                                                   METALLICITIES_PER_THOUSAND,
+                                                   FILES_PATHS)
 from .utils import (read_sequences_by_metallicity_from_fortran,
                     values_by_metallicity_are_close)
 
@@ -9,9 +10,13 @@ import db_cooling
 
 def test_db_cooling_table() -> None:
     db_cooling_table_by_python = table.read(table_name='db_cooling')
+
+    files_paths = [FILES_PATHS[metallicity]
+                   for metallicity in METALLICITIES_PER_THOUSAND]
+    files_counts_per_metallicity = list(map(len, files_paths))
     db_cooling_table_by_fortran = read_sequences_by_metallicity_from_fortran(
         rows_count=ROWS_COUNT,
-        files_counts_per_metallicity=[7, 9, 9],
+        files_counts_per_metallicity=files_counts_per_metallicity,
         fill_types=[1, 2, 3],
         fort_files_initial_units=[90, 100, 110],
         metallicities_by_thousand=METALLICITIES_PER_THOUSAND,
