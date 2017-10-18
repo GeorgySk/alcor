904feec9328ce3f542200a1b0e9a232b09c39060
Azat Ibrakov <azatibrakov@gmail.com>
2017-10-18 23:00:02 +0700

    Completed parsing 'Star' instances from strings

--- alcor/utils.py
+++ alcor/utils.py
@@ -7,8 +7,9 @@ from typing import (Any,
                     Hashable,
                     Iterable,
                     Iterator,
+                    Container,
                     Mapping,
-                    Dict)
+                    Dict, Tuple)
 
 import yaml
 
@@ -31,13 +32,14 @@ def join_str(items: Iterable[Any],
 
 
 def parse_stars(lines: Iterator[str],
-                group: Group) -> Iterator[Star]:
+                *,
+                group: Group,
+                possible_columns_names: Container[str] = STAR_PARAMETERS_NAMES
+                ) -> Iterator[Star]:
     header = next(lines).split()
-    for column_name in header:
-        if column_name not in STAR_PARAMETERS_NAMES:
-            logger.error('There is no parameter "{column_name}" in '
-                         'STAR_PARAMETERS_NAMES'
-                         .format(column_name=column_name))
+    validate_header(header,
+                    possible_columns_names=possible_columns_names)
+
     for line in lines:
         parts = line.split()
         params = map(str_to_float, parts)
@@ -47,6 +49,18 @@ def parse_stars(lines: Iterator[str],
                    **values)
 
 
+def validate_header(header: Iterable[str],
+                    *,
+                    possible_columns_names: Container[str]) -> None:
+    unknown_columns_names = [column_name
+                             for column_name in header
+                             if column_name not in possible_columns_names]
+    if unknown_columns_names:
+        err_msg = ('Unknown columns names: "{columns_names}".'
+                   .format(columns_names='", "'.join(unknown_columns_names)))
+        raise ValueError(err_msg)
+
+
 def str_to_float(string: str) -> Union[str, float]:
     try:
         return float(string)
@@ -54,7 +68,8 @@ def str_to_float(string: str) -> Union[str, float]:
         return string
 
 
-def zip_mappings(*mappings: Mapping[Hashable, Any]):
+def zip_mappings(*mappings: Mapping[Hashable, Any]
+                 ) -> Iterator[Tuple[Hashable, Tuple[Any, ...]]]:
     keys_sets = map(set, mappings)
     common_keys = reduce(set.intersection, keys_sets)
     for key in common_keys:
