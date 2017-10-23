b7f82fdd1186adf2cbe9616dc771ed587134b414
Georgy Skorobogatov <skorobogatov@phystech.edu>
2017-10-23 18:03:11 +0200

    Implemented proper tests for `str_to_float` function using strategies

--- tests/strategies/__init__.py
+++ tests/strategies/__init__.py
@@ -2,3 +2,5 @@ from .processing import filtration_methods
 from .stars import (defined_stars,
                     defined_stars_lists,
                     undefined_stars_lists)
+from .utils import (floats,
+                    non_float_strings)
