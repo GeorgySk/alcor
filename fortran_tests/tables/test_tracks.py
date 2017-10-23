e026463f466aa88df824470ecc11c9a489b3c687
Georgy Skorobogatov <skorobogatov@phystech.edu>
2017-10-23 14:39:37 +0200

    Added typings in `test_tracks` signature

--- fortran_tests/tables/test_tracks.py
+++ fortran_tests/tables/test_tracks.py
@@ -1,15 +1,16 @@
+from typing import List
 
 
-def test_tracks(da_cooling_tracks_lengths,
-                db_cooling_tracks_lengths,
-                da_colors_tracks_lengths,
-                db_colors_tracks_lengths,
-                one_tables_lengths,
-                da_cooling_fort_files_lengths,
-                db_cooling_fort_files_lengths,
-                da_colors_fort_files_lengths,
-                db_colors_fort_files_lengths,
-                one_fort_files_lengths) -> None:
+def test_tracks(da_cooling_tracks_lengths: List[int],
+                db_cooling_tracks_lengths: List[int],
+                da_colors_tracks_lengths: List[int],
+                db_colors_tracks_lengths: List[int],
+                one_tables_lengths: List[int],
+                da_cooling_fort_files_lengths: List[int],
+                db_cooling_fort_files_lengths: List[int],
+                da_colors_fort_files_lengths: List[int],
+                db_colors_fort_files_lengths: List[int],
+                one_fort_files_lengths: List[int]) -> None:
     assert da_cooling_tracks_lengths == da_cooling_fort_files_lengths
     assert db_cooling_tracks_lengths == db_cooling_fort_files_lengths
     assert da_colors_tracks_lengths == da_colors_fort_files_lengths
