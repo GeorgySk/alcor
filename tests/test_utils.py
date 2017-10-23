eb9a5bc86d95e8bc7ed75325123c43797d3858c7
Georgy Skorobogatov <skorobogatov@phystech.edu>
2017-10-23 18:03:11 +0200

    Implemented proper tests for `str_to_float` function using strategies

---
+++ tests/test_utils.py
@@ -0,0 +1,16 @@
+import math
+
+from alcor.utils import str_to_float
+
+
+def test_str_to_float(float_value: float,
+                      float_value_str: str,
+                      non_float_string: str):
+    float_result = str_to_float(float_value_str)
+    non_float_result = str_to_float(non_float_string)
+
+    assert isinstance(float_result, float)
+    assert isinstance(non_float_result, str)
+    assert math.isclose(float_result,
+                        float_value)
+    assert non_float_result == non_float_string
