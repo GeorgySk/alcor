3a6f6219bb6f08bdfddaad6a4134617229359064
Georgy Skorobogatov <skorobogatov@phystech.edu>
2017-10-26 21:26:43 +0200

    Reformatting docstring

--- alcor/services/simulations/luminosities.py
+++ alcor/services/simulations/luminosities.py
@@ -11,22 +11,22 @@ def get_white_dwarfs(stars: pd.DataFrame,
                      max_mass: float = 10.5,
                      solar_metallicity: float = 0.01,
                      subsolar_metallicity: float = 0.001) -> pd.DataFrame:
-    """Filter white dwarfs stars(WD) from initial sample of main sequence stars
-    and assign metallicities, cooling times and masses.
-
-    Keyword arguments:
-    stars -- data frame with main sequence stars
-    max_galactic_structure_age -- the highest age of thin disk, thick disk
-                                  and halo
-    ifmr_parameter -- factor by which WD mass (calculated from Initial-to-Final
-                      Mass Relation (IFMR)) is multiplied (default 1.)
-    chandrasekhar_limit -- maximum mass of a stable WD (default 1.4)
-    max_mass -- maximum mass of a main sequence star that can generate a WD
-                (default 10.5)
-    solar_metallicity -- metallicity assigned to all thin and thick disks WDs
-                         due to relatively young ages (default 0.01)
-    subsolar_metallicity -- metallicity assigned to all halo WDs
-                            (default 0.001)
+    """
+    Filters white dwarfs stars(WD) from initial sample of main sequence stars
+    and assigns metallicities, cooling times and masses.
+    :param stars: data frame with main sequence stars
+    :param max_galactic_structure_age: the highest age of thin disk, thick disk
+    and halo
+    :param ifmr_parameter: factor by which WD mass (calculated from
+    Initial-to-Final Mass Relation (IFMR)) is multiplied (default 1.)
+    :param chandrasekhar_limit: maximum mass of a stable WD (default 1.4)
+    :param max_mass: maximum mass of a main sequence star that can generate
+    a WD (default 10.5)
+    :param solar_metallicity: metallicity assigned to all thin and thick disks
+    WDs due to relatively young ages (default 0.01)
+    :param subsolar_metallicity: metallicity assigned to all halo WDs
+    (default 0.001)
+    :return: data frame with white of white dwarfs
     """
     stars = filter_by_max_mass(stars,
                                max_mass=max_mass)
@@ -103,8 +103,9 @@ def get_main_sequence_lifetimes(*,
                                 metallicities: np.ndarray,
                                 solar_metallicity: float,
                                 subsolar_metallicity: float) -> np.ndarray:
-    """Calculate lifetime of a main sequence star
-    according to model by Leandro & Renedo et al.(2010).
+    """
+    Calculates lifetime of a main sequence star according to model by
+    Leandro & Renedo et al.(2010).
     Solar metallicity values from Althaus priv. comm (X = 0.725, Y = 0.265)
     Sub-solar metallicity values from Althaus priv. comm (X = 0.752, Y = 0.247)
     """
@@ -161,8 +162,10 @@ def extrapolated_times(*,
                        masses: np.ndarray,
                        rightmost_mass: float,
                        rightmost_time: float) -> np.ndarray:
-    """Extrapolate main sequence stars (progenitors) lifetime vs mass to the
-    right. Make sure that no negative values will be produced."""
+    """
+    Extrapolates main sequence stars (progenitors) lifetime vs mass to the
+    right. Makes sure that no negative values will be produced.
+    """
     return rightmost_time * rightmost_mass / masses
 
 
