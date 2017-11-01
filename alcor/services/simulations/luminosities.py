e933650678282908e0adae5e5362d54cd884fec4
Georgy Skorobogatov <skorobogatov@phystech.edu>
2017-11-01 20:33:20 +0100

    Reformatting docstrings

--- alcor/services/simulations/luminosities.py
+++ alcor/services/simulations/luminosities.py
@@ -13,23 +13,21 @@ def get_white_dwarfs(stars: pd.DataFrame,
                      subsolar_metallicity: float = 0.001,
                      min_cooling_time: float = 0.) -> pd.DataFrame:
     """
-    Filters white dwarfs stars(WD) from initial sample of main sequence stars
+    Filters white dwarfs stars from initial sample of main sequence stars
     and assigns metallicities, cooling times and masses.
 
-    :param stars: data frame with main sequence stars
-    :param max_galactic_structure_age: the highest age of thin disk, thick disk
-    and halo
-    :param ifmr_parameter: factor by which WD mass (calculated from
-    Initial-to-Final Mass Relation (IFMR)) is multiplied (default 1.)
-    :param chandrasekhar_limit: maximum mass of a stable WD (default 1.4)
-    :param max_mass: maximum mass of a main sequence star that can generate
-    a WD (default 10.5)
-    :param solar_metallicity: metallicity assigned to all thin and thick disks
-    WDs due to relatively young ages (default 0.01)
-    :param subsolar_metallicity: metallicity assigned to all halo WDs
-    (default 0.001)
-    :param min_cooling_time: natural lower limit for cooling time (default 0.0)
-    :return: data frame with white of white dwarfs
+    :param stars: main sequence stars
+    :param max_galactic_structure_age: the highest age of thin disk,
+    thick disk and halo
+    :param ifmr_parameter: factor by which white dwarf's mass is multiplied
+    :param chandrasekhar_limit: maximum mass of a stable white dwarf
+    :param max_mass: maximum mass of a main sequence star
+    that can generate a white dwarf
+    :param solar_metallicity: metallicity assigned to all thin
+    and thick disks white dwarfs due to relatively young ages
+    :param subsolar_metallicity: metallicity assigned to all halo white dwarfs
+    :param min_cooling_time: natural lower limit for cooling time
+    :return: white dwarfs
     """
     stars = stars[stars['progenitor_mass'] < max_mass]
     set_metallicities(stars,
@@ -88,8 +86,8 @@ def get_main_sequence_lifetimes(*,
                                 solar_metallicity: float,
                                 subsolar_metallicity: float) -> np.ndarray:
     """
-    Calculates lifetime of a main sequence star according to model by
-    Leandro & Renedo et al.(2010).
+    Calculates lifetime of a main sequence star
+    according to model by Leandro & Renedo et al.(2010).
     Solar metallicity values from Althaus priv. comm (X = 0.725, Y = 0.265)
     Sub-solar metallicity values from Althaus priv. comm (X = 0.752, Y = 0.247)
     """
@@ -147,8 +145,9 @@ def extrapolated_times(*,
                        rightmost_mass: float,
                        rightmost_time: float) -> np.ndarray:
     """
-    Extrapolates main sequence stars (progenitors) lifetime vs mass
-    to the right. Makes sure that no negative values will be produced.
+    Extrapolates main sequence stars' (progenitors)
+    lifetime vs mass to the right.
+    Makes sure that no negative values will be produced.
     """
     return rightmost_time * rightmost_mass / masses
 
