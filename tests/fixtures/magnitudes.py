d4ad67003140396a7c62a82553de6a2827de07b3
Georgy Skorobogatov <skorobogatov@phystech.edu>
2017-11-12 21:34:04 +0100

    Add tests

--- tests/fixtures/magnitudes.py
+++ tests/fixtures/magnitudes.py
@@ -1,7 +1,10 @@
 import random
-from typing import Tuple, List
+from typing import (Dict,
+                    Tuple,
+                    List)
 
 import numpy as np
+import pandas as pd
 import pytest
 
 from tests.utils import example
@@ -68,3 +71,278 @@ def db_to_da_fraction() -> float:
 @pytest.fixture(scope='function')
 def size() -> int:
     return 10
+
+
+@pytest.fixture(scope='function')
+def mass() -> float:
+    return random.uniform(0.4, 1.4)
+
+
+@pytest.fixture(scope='function')
+def greater_mass_cooling_time_grid() -> np.ndarray:
+    return np.linspace(1., 10., 10)
+
+
+@pytest.fixture(scope='function')
+def lesser_mass_cooling_time_grid() -> np.ndarray:
+    return np.linspace(0., 9., 10)
+
+
+@pytest.fixture(scope='function')
+def greater_mass_interest_parameter_grid() -> np.ndarray:
+    return np.linspace(-12., 4., 10)
+
+
+@pytest.fixture(scope='function')
+def lesser_mass_interest_parameter_grid() -> np.ndarray:
+    return np.linspace(-15., 1., 10)
+
+
+@pytest.fixture(scope='function')
+def min_mass() -> float:
+    return 0.7
+
+
+@pytest.fixture(scope='function')
+def max_mass() -> float:
+    return 1.3
+
+
+@pytest.fixture(scope='function')
+def min_row_index() -> int:
+    return 2
+
+
+@pytest.fixture(scope='function')
+def max_row_index() -> int:
+    return 3
+
+
+@pytest.fixture(scope='function')
+def star_series() -> pd.Series:
+    return pd.Series(dict(mass=1.,
+                          luminosity=0.,
+                          metallicity=0.01,
+                          cooling_time=4.))
+
+
+@pytest.fixture(scope='function')
+def color_table() -> Dict[int, pd.DataFrame]:
+    return {52400: pd.DataFrame(dict(
+                        u_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                        luminosity=np.array([-2., -1., 0., 1.]))),
+            100000: pd.DataFrame(dict(
+                        u_ubvri_absolute=np.array([-15., -5., 8., 20.]),
+                        luminosity=np.array([-3., -1., 0., 2.])))}
+
+
+@pytest.fixture(scope='function')
+def color() -> str:
+    return 'u_ubvri_absolute'
+
+
+@pytest.fixture(scope='function')
+def tracks() -> Dict[int, pd.DataFrame]:
+    return {52400: pd.DataFrame(dict(
+                u_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                luminosity=np.array([-2., -1., 0., 1.]),
+                cooling_time=np.array([3., 4., 5., 6.]))),
+            100000: pd.DataFrame(dict(
+                u_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                luminosity=np.array([-3., -1., 0., 2.]),
+                cooling_time=np.array([2., 4., 6., 8.])))}
+
+
+@pytest.fixture(scope='function')
+def interest_parameter() -> str:
+    return 'u_ubvri_absolute'
+
+
+@pytest.fixture(scope='function')
+def metallicity_grid() -> List[float]:
+    return [0.001, 0.01, 0.03, 0.06]
+
+
+@pytest.fixture(scope='function')
+def cooling_sequences() -> Dict[int, Dict[int, pd.DataFrame]]:
+    return {1: {52400: pd.DataFrame(dict(
+                    u_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                    luminosity=np.array([-2., -1., 0., 1.]),
+                    cooling_time=np.array([3., 4., 5., 6.]))),
+                100000: pd.DataFrame(dict(
+                    u_ubvri_absolute=np.array([-15., -5., 8., 20.]),
+                    luminosity=np.array([-3., -1., 0., 2.]),
+                    cooling_time=np.array([2., 4., 6., 8.])))},
+            10: {52400: pd.DataFrame(dict(
+                    u_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                    luminosity=np.array([-2., -1., 0., 1.]),
+                    cooling_time=np.array([3., 4., 5., 6.]))),
+                 100000: pd.DataFrame(dict(
+                    u_ubvri_absolute=np.array([-15., -5., 8., 20.]),
+                    luminosity=np.array([-3., -1., 0., 2.]),
+                    cooling_time=np.array([2., 4., 6., 8.])))},
+            30: {52400: pd.DataFrame(dict(
+                    u_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                    luminosity=np.array([-2., -1., 0., 1.]),
+                    cooling_time=np.array([3., 4., 5., 6.]))),
+                 100000: pd.DataFrame(dict(
+                    u_ubvri_absolute=np.array([-15., -5., 8., 20.]),
+                    luminosity=np.array([-3., -1., 0., 2.]),
+                    cooling_time=np.array([2., 4., 6., 8.])))},
+            60: {52400: pd.DataFrame(dict(
+                    u_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                    luminosity=np.array([-2., -1., 0., 1.]),
+                    cooling_time=np.array([3., 4., 5., 6.]))),
+                 100000: pd.DataFrame(dict(
+                    u_ubvri_absolute=np.array([-15., -5., 8., 20.]),
+                    luminosity=np.array([-3., -1., 0., 2.]),
+                    cooling_time=np.array([2., 4., 6., 8.])))}}
+
+
+@pytest.fixture(scope='function')
+def stars() -> pd.DataFrame:
+    return pd.DataFrame(dict(mass=[1., 1.1],
+                             luminosity=[0., 1.],
+                             metallicity=[0.01, 0.06],
+                             cooling_time=[3., 4.]))
+
+
+@pytest.fixture(scope='function')
+def stars_without_luminosity() -> pd.DataFrame:
+    return pd.DataFrame(dict(mass=[1., 1.1],
+                             metallicity=[0.01, 0.06],
+                             cooling_time=[3., 4.]))
+
+
+@pytest.fixture(scope='function')
+def da_cooling_sequences() -> Dict[int, Dict[int, pd.DataFrame]]:
+    return {1: {52400: pd.DataFrame(dict(
+                    u_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                    luminosity=np.array([-2., -1., 0., 1.]),
+                    cooling_time=np.array([3., 4., 5., 6.]),
+                    effective_temperature=np.array([1e4, 1.5e4, 2e4, 3e4]))),
+                100000: pd.DataFrame(dict(
+                    u_ubvri_absolute=np.array([-15., -5., 8., 20.]),
+                    luminosity=np.array([-3., -1., 0., 2.]),
+                    cooling_time=np.array([2., 4., 6., 8.]),
+                    effective_temperature=np.array([1e4, 1.5e4, 2e4, 3e4])))},
+            10: {52400: pd.DataFrame(dict(
+                    u_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                    luminosity=np.array([-2., -1., 0., 1.]),
+                    cooling_time=np.array([3., 4., 5., 6.]),
+                    effective_temperature=np.array([1e4, 1.5e4, 2e4, 3e4]))),
+                 100000: pd.DataFrame(dict(
+                    u_ubvri_absolute=np.array([-15., -5., 8., 20.]),
+                    luminosity=np.array([-3., -1., 0., 2.]),
+                    cooling_time=np.array([2., 4., 6., 8.]),
+                    effective_temperature=np.array([1e4, 1.5e4, 2e4, 3e4])))},
+            30: {52400: pd.DataFrame(dict(
+                    u_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                    luminosity=np.array([-2., -1., 0., 1.]),
+                    cooling_time=np.array([3., 4., 5., 6.]),
+                    effective_temperature=np.array([1e4, 1.5e4, 2e4, 3e4]))),
+                 100000: pd.DataFrame(dict(
+                    u_ubvri_absolute=np.array([-15., -5., 8., 20.]),
+                    luminosity=np.array([-3., -1., 0., 2.]),
+                    cooling_time=np.array([2., 4., 6., 8.]),
+                    effective_temperature=np.array([1e4, 1.5e4, 2e4, 3e4])))},
+            60: {52400: pd.DataFrame(dict(
+                    u_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                    luminosity=np.array([-2., -1., 0., 1.]),
+                    cooling_time=np.array([3., 4., 5., 6.]),
+                    effective_temperature=np.array([1e4, 1.5e4, 2e4, 3e4]))),
+                 100000: pd.DataFrame(dict(
+                    u_ubvri_absolute=np.array([-15., -5., 8., 20.]),
+                    luminosity=np.array([-3., -1., 0., 2.]),
+                    cooling_time=np.array([2., 4., 6., 8.]),
+                    effective_temperature=np.array([1e4, 1.5e4, 2e4, 3e4])))}}
+
+
+@pytest.fixture(scope='function')
+def db_cooling_sequences() -> Dict[int, Dict[int, pd.DataFrame]]:
+    return {1: {52400: pd.DataFrame(dict(
+                    u_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                    luminosity=np.array([-2., -1., 0., 1.]),
+                    cooling_time=np.array([3., 4., 5., 6.]),
+                    effective_temperature=np.array([1e4, 1.5e4, 2e4, 3e4]))),
+                100000: pd.DataFrame(dict(
+                    u_ubvri_absolute=np.array([-15., -5., 8., 20.]),
+                    luminosity=np.array([-3., -1., 0., 2.]),
+                    cooling_time=np.array([2., 4., 6., 8.]),
+                    effective_temperature=np.array([1e4, 1.5e4, 2e4, 3e4])))},
+            10: {52400: pd.DataFrame(dict(
+                    u_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                    luminosity=np.array([-2., -1., 0., 1.]),
+                    cooling_time=np.array([3., 4., 5., 6.]),
+                    effective_temperature=np.array([1e4, 1.5e4, 2e4, 3e4]))),
+                 100000: pd.DataFrame(dict(
+                    u_ubvri_absolute=np.array([-15., -5., 8., 20.]),
+                    luminosity=np.array([-3., -1., 0., 2.]),
+                    cooling_time=np.array([2., 4., 6., 8.]),
+                    effective_temperature=np.array([1e4, 1.5e4, 2e4, 3e4])))},
+            60: {52400: pd.DataFrame(dict(
+                    u_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                    luminosity=np.array([-2., -1., 0., 1.]),
+                    cooling_time=np.array([3., 4., 5., 6.]),
+                    effective_temperature=np.array([1e4, 1.5e4, 2e4, 3e4]))),
+                 100000: pd.DataFrame(dict(
+                    u_ubvri_absolute=np.array([-15., -5., 8., 20.]),
+                    luminosity=np.array([-3., -1., 0., 2.]),
+                    cooling_time=np.array([2., 4., 6., 8.]),
+                    effective_temperature=np.array([1e4, 1.5e4, 2e4, 3e4])))}}
+
+
+@pytest.fixture(scope='function')
+def da_color_table() -> Dict[int, pd.DataFrame]:
+    return {52400: pd.DataFrame(dict(
+                    u_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                    b_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                    v_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                    r_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                    i_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                    j_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                    luminosity=np.array([-2., -1., 0., 1.]),
+                    cooling_time=np.array([3., 4., 5., 6.]))),
+            100000: pd.DataFrame(dict(
+                    u_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                    b_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                    v_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                    r_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                    i_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                    j_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                    luminosity=np.array([-3., -1., 0., 2.]),
+                    cooling_time=np.array([2., 4., 6., 8.])))}
+
+
+@pytest.fixture(scope='function')
+def db_color_table() -> Dict[int, pd.DataFrame]:
+    return {52400: pd.DataFrame(dict(
+                    u_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                    b_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                    v_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                    r_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                    i_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                    j_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                    luminosity=np.array([-2., -1., 0., 1.]),
+                    cooling_time=np.array([3., 4., 5., 6.]))),
+            100000: pd.DataFrame(dict(
+                    u_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                    b_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                    v_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                    r_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                    i_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                    j_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                    luminosity=np.array([-3., -1., 0., 2.]),
+                    cooling_time=np.array([2., 4., 6., 8.])))}
+
+
+@pytest.fixture(scope='function')
+def one_color_table() -> Dict[int, pd.DataFrame]:
+    return {52400: pd.DataFrame(dict(
+                    u_ubvri_absolute=np.array([-10., 0., 10., 20.]),
+                    luminosity=np.array([-2., -1., 0., 1.]),
+                    cooling_time=np.array([3., 4., 5., 6.]))),
+            100000: pd.DataFrame(dict(
+                    u_ubvri_absolute=np.array([-15., -5., 8., 20.]),
+                    luminosity=np.array([-3., -1., 0., 2.]),
+                    cooling_time=np.array([2., 4., 6., 8.])))}
--- tests/test_magnitudes.py
+++ tests/test_magnitudes.py
@@ -1,14 +1,22 @@
-from typing import (Tuple,
+from typing import (Dict,
+                    Tuple,
                     List)
 
 import numpy as np
+import pandas as pd
 
 from alcor.services.simulations.magnitudes import (estimate_at,
                                                    estimated_interest_value,
                                                    calculate_index,
                                                    extrapolating_by_grid,
                                                    get_min_metallicity_index,
-                                                   generate_spectral_types)
+                                                   generate_spectral_types,
+                                                   extrapolate_interest_value,
+                                                   interpolate_interest_value,
+                                                   estimate_color,
+                                                   estimate_by_mass,
+                                                   estimate_by_metallicities,
+                                                   assign_estimated_values)
 
 
 def test_estimate_at(float_value: float,
@@ -67,3 +75,111 @@ def test_generate_spectral_types(db_to_da_fraction: float,
             size=size)
 
     assert isinstance(spectral_types, np.ndarray)
+
+
+def test_extrapolate_interest_value(
+        mass: float,
+        cooling_time: float,
+        greater_mass_cooling_time_grid: np.ndarray,
+        greater_mass_interest_parameter_grid: np.ndarray,
+        lesser_mass_cooling_time_grid: np.ndarray,
+        lesser_mass_interest_parameter_grid: np.ndarray,
+        min_mass: float,
+        max_mass: float,
+        min_row_index: int,
+        max_row_index: int) -> None:
+    value = extrapolate_interest_value(
+            mass=mass,
+            cooling_time=cooling_time,
+            greater_mass_cooling_time_grid=greater_mass_cooling_time_grid,
+            greater_mass_interest_parameter_grid=(
+                greater_mass_interest_parameter_grid),
+            lesser_mass_cooling_time_grid=lesser_mass_cooling_time_grid,
+            lesser_mass_interest_parameter_grid=(
+                lesser_mass_interest_parameter_grid),
+            min_mass=min_mass,
+            max_mass=max_mass,
+            min_row_index=min_row_index,
+            max_row_index=max_row_index)
+
+    assert isinstance(value, float)
+
+
+def test_interpolate_interest_value(
+        mass: float,
+        cooling_time: float,
+        greater_mass_cooling_time_grid: np.ndarray,
+        greater_mass_interest_parameter_grid: np.ndarray,
+        lesser_mass_cooling_time_grid: np.ndarray,
+        lesser_mass_interest_parameter_grid: np.ndarray,
+        min_mass: float,
+        max_mass: float,
+        min_row_index: int,
+        max_row_index: int) -> None:
+    value = interpolate_interest_value(
+            mass=mass,
+            cooling_time=cooling_time,
+            greater_mass_cooling_time_grid=greater_mass_cooling_time_grid,
+            greater_mass_interest_parameter_grid=(
+                greater_mass_interest_parameter_grid),
+            lesser_mass_cooling_time_grid=lesser_mass_cooling_time_grid,
+            lesser_mass_interest_parameter_grid=(
+                lesser_mass_interest_parameter_grid),
+            min_mass=min_mass,
+            max_mass=max_mass,
+            min_row_index=min_row_index,
+            max_row_index=max_row_index)
+
+    assert isinstance(value, float)
+
+
+def test_estimate_color(star_series: pd.Series,
+                        color_table: Dict[int, pd.DataFrame],
+                        color: str) -> None:
+    color = estimate_color(star_series,
+                           color_table=color_table,
+                           color=color)
+
+    isinstance(color, float)
+
+
+def test_estimate_by_mass(star_series: pd.Series,
+                          tracks: Dict[int, pd.DataFrame],
+                          interest_parameter: str) -> None:
+    parameter = estimate_by_mass(star_series,
+                                 tracks=tracks,
+                                 interest_parameter=interest_parameter)
+
+    isinstance(parameter, float)
+
+
+def test_estimate_by_metallicities(
+        star_series: pd.Series,
+        metallicity_grid: List[float],
+        cooling_sequences: Dict[int, Dict[int, pd.DataFrame]],
+        interest_parameter: str) -> None:
+    parameter = estimate_by_metallicities(
+            star_series,
+            metallicity_grid=metallicity_grid,
+            cooling_sequences=cooling_sequences,
+            interest_parameter=interest_parameter)
+
+    isinstance(parameter, float)
+
+
+def test_assign_estimated_values(
+        stars_without_luminosity: pd.DataFrame,
+        da_cooling_sequences: Dict[int, Dict[int, pd.DataFrame]],
+        da_color_table: Dict[int, pd.DataFrame],
+        db_cooling_sequences: Dict[int, Dict[int, pd.DataFrame]],
+        db_color_table: Dict[int, pd.DataFrame],
+        one_color_table: Dict[int, pd.DataFrame]) -> None:
+    stars_without_luminosity = assign_estimated_values(
+            stars_without_luminosity,
+            da_cooling_sequences=da_cooling_sequences,
+            da_color_table=da_color_table,
+            db_cooling_sequences=db_cooling_sequences,
+            db_color_table=db_color_table,
+            one_color_table=one_color_table)
+
+    isinstance(stars_without_luminosity, pd.DataFrame)
