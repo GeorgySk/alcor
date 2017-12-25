fead02721e9dcd71a14023e29ad849f47b442972
Georgy Skorobogatov <skorobogatov@phystech.edu>
2017-12-25 18:18:09 +0100

    Add test for `halo_stars_velocities`

--- tests/fixtures/velocities.py
+++ tests/fixtures/velocities.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pytest
 
+from alcor.types import GaussianGeneratorType
 from tests.utils import example
 from tests import strategies
 
@@ -18,3 +19,44 @@ def y_values(x_values: np.ndarray) -> np.ndarray:
 @pytest.fixture(scope='function')
 def angles(x_values: np.ndarray) -> np.ndarray:
     return example(strategies.floats_arrays_w_size(shape=x_values.shape))
+
+
+@pytest.fixture(scope='function')
+def galactic_longitudes() -> np.ndarray:
+    return example(strategies.floats_arrays)
+
+
+@pytest.fixture(scope='function')
+def thetas_cylindrical(galactic_longitudes: np.ndarray) -> np.ndarray:
+    shape = galactic_longitudes.shape
+    return example(strategies.floats_arrays_w_size(shape=shape))
+
+
+@pytest.fixture(scope='function')
+def u_peculiar_solar_velocity() -> float:
+    return example(strategies.floats)
+
+
+@pytest.fixture(scope='function')
+def v_peculiar_solar_velocity() -> float:
+    return example(strategies.floats)
+
+
+@pytest.fixture(scope='function')
+def w_peculiar_solar_velocity() -> float:
+    return example(strategies.floats)
+
+
+@pytest.fixture(scope='function')
+def lsr_velocity() -> float:
+    return example(strategies.floats)
+
+
+@pytest.fixture(scope='function')
+def spherical_velocity_component_sigma() -> float:
+    return example(strategies.floats)
+
+
+@pytest.fixture(scope='function')
+def gaussian_generator() -> GaussianGeneratorType:
+    return np.random.normal
--- tests/test_velocities.py
+++ tests/test_velocities.py
@@ -1,6 +1,8 @@
 import numpy as np
 
-from alcor.services.simulations.velocities import rotate_vectors
+from alcor.services.simulations.velocities import (rotate_vectors,
+                                                   halo_stars_velocities)
+from alcor.types import GaussianGeneratorType
 
 
 def test_rotate_vectors(*,
@@ -15,3 +17,31 @@ def test_rotate_vectors(*,
     assert isinstance(rotated_y_values, np.ndarray)
     assert rotated_x_values.size == x_values.size
     assert rotated_y_values.size == y_values.size
+
+
+def test_halo_stars_velocities(galactic_longitudes: np.ndarray,
+                               thetas_cylindrical: np.ndarray,
+                               u_peculiar_solar_velocity: float,
+                               v_peculiar_solar_velocity: float,
+                               w_peculiar_solar_velocity: float,
+                               lsr_velocity: float,
+                               spherical_velocity_component_sigma: float,
+                               gaussian_generator: GaussianGeneratorType
+                               ) -> None:
+    x_velocities, y_velocities, z_velocities = halo_stars_velocities(
+            galactic_longitudes=galactic_longitudes,
+            thetas_cylindrical=thetas_cylindrical,
+            u_peculiar_solar_velocity=u_peculiar_solar_velocity,
+            v_peculiar_solar_velocity=v_peculiar_solar_velocity,
+            w_peculiar_solar_velocity=w_peculiar_solar_velocity,
+            lsr_velocity=lsr_velocity,
+            spherical_velocity_component_sigma=(
+                spherical_velocity_component_sigma),
+            generator=gaussian_generator)
+
+    assert isinstance(x_velocities, np.ndarray)
+    assert isinstance(y_velocities, np.ndarray)
+    assert isinstance(z_velocities, np.ndarray)
+    assert x_velocities.size == galactic_longitudes.size
+    assert y_velocities.size == galactic_longitudes.size
+    assert z_velocities.size == galactic_longitudes.size
