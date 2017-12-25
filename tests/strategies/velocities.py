9de585c7ccf4120fd74cf4c334a8acc091d57d3d
Georgy Skorobogatov <skorobogatov@phystech.edu>
2017-12-25 19:07:12 +0100

    Increase size of arrays from 10 to 100

--- tests/strategies/velocities.py
+++ tests/strategies/velocities.py
@@ -4,7 +4,7 @@ from hypothesis import strategies
 from hypothesis.extra.numpy import arrays
 
 arrays_sizes = strategies.integers(min_value=0,
-                                   max_value=10)
+                                   max_value=100)
 
 floats_arrays = arrays(dtype=float,
                        shape=arrays_sizes)
